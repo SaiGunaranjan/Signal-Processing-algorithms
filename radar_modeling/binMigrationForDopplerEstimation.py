Fix bug in the Doppler bin computation.
Previosuly, I was taking modulo Fs/2 of the velocity and post this, I
was converting the velocity to bipolar. This is wrong. In this commit, I have fixed this bug. Now, I take the modulo of Fs of the velocity and the compute the bin as is without making it bi-polar. Also, in this commit, I have put all the constants like system parameters at the beginning of the code. This make the code a lot cleaner.

Signed-off-by: Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>

radar_modeling/binMigrationForDopplerEstimation.py
@@ -11,22 +11,40 @@ import matplotlib.pyplot as plt
 plt.close('all')
 # np.random.seed(0)
 
+""" System Parameters """
 numSamples = 1024
 numFFTBins = 1024 #8192
-# print('Num FFT points = {}'.format(numFFTBins))
-fftOverSamplingFact = numFFTBins//numSamples
 thermalNoise = -174 # dBm/Hz
 noiseFigure = 10 # dB
 baseBandgain = 34 #dB
 adcSamplingRate = 56.25e6 # 56.25 MHz
-adcSamplingTime = 1/adcSamplingRate # seconds
 dBFs_to_dBm = 10
+binSNR = 20 # dB
+lightSpeed = 3e8 # m/s
+
+""" Chirp Parameters"""
+chirpBW = 4e9 # Hz
+interRampTime = 44e-6 # us
+chirpOnTime = 39.2e-6
+chirpStartFreq = 77e9 # Giga Hz
+numChirps = 168
+
+
+""" Object parameters"""
+objectRange_m = 10
+objectVelocity_mps = 40 # m/s
+
+
+
+fftOverSamplingFact = numFFTBins//numSamples
+adcSamplingTime = 1/adcSamplingRate # seconds
+
 totalNoisePower_dBm = thermalNoise + noiseFigure + baseBandgain + 10*np.log10(adcSamplingRate)
 totalNoisePower_dBFs = totalNoisePower_dBm - 10
 noiseFloor_perBin = totalNoisePower_dBFs - 10*np.log10(numFFTBins) # dBFs/bin
 # noiseFloor_perBin = -100 # dBFs
 noisePower_perBin = 10**(noiseFloor_perBin/10)
-binSNR = 20 # dB
+
 
 totalNoisePower = noisePower_perBin*numFFTBins # sigmasquare totalNoisePower
 sigma = np.sqrt(totalNoisePower)
@@ -35,40 +53,37 @@ signalPower = 10**(signalPowerdBFs/10)
 signalAmplitude = np.sqrt(signalPower)
 signalPhase = np.exp(1j*np.random.uniform(-np.pi, np.pi))
 signalphasor = signalAmplitude*signalPhase
-objectRange_m = 10
-chirpBW = 4e9 # Hz
-lightSpeed = 3e8 # m/s
+
 rangeRes = lightSpeed/(2*chirpBW)
 rangeAxis_m = np.arange(numFFTBins//2)*rangeRes
 objectRangeBin = objectRange_m/rangeRes
 objectRangeBinInt = np.int(objectRangeBin)
 rangeSignal = signalphasor*np.exp(1j*((2*np.pi*objectRangeBin)/numSamples)*np.arange(numSamples))
 
-""" Chirp Parameters"""
-interRampTime = 44e-6 # us
+
 chirpSamplingRate = 1/interRampTime
-chirpOnTime = 39.2e-6
 chirpSlope = chirpBW/chirpOnTime
-chirpStartFreq = 77e9 # Giga Hz
 chirpCentreFreq = chirpStartFreq + chirpBW/2
 lamda = lightSpeed/chirpCentreFreq
 maxVelBaseband_mps = (chirpSamplingRate/2) * (lamda/2) # m/s
-numChirps = 168
-objectVelocity_mps = 40 # m/s
-objectVelocity_baseBand_mps = np.mod(objectVelocity_mps, maxVelBaseband_mps)
-if (objectVelocity_baseBand_mps >= maxVelBaseband_mps/2):
-    objectVelocity_baseBand_mps = objectVelocity_baseBand_mps - maxVelBaseband_mps
-
+FsEquivalentVelocity = 2*maxVelBaseband_mps # Fs = 2*Fs/2
 velocityRes = (chirpSamplingRate/numChirps) * (lamda/2)
+
+objectVelocity_baseBand_mps = np.mod(objectVelocity_mps, FsEquivalentVelocity) # modulo Fs [from 0 to Fs]
 objectVelocityBin = objectVelocity_baseBand_mps/velocityRes
 objectVelocityInt = np.int(objectVelocityBin)
 
+
+# if (objectVelocity_baseBand_mps >= maxVelBaseband_mps):
+#     objectVelocity_baseBand_mps = objectVelocity_baseBand_mps - FsEquivalentVelocity
+
 dopplerSignal = np.exp(1j*((2*np.pi*objectVelocityBin)/numChirps)*np.arange(numChirps))
 
-rangeBinMigrationTerm = \
+""" Range Bin migration term"""
+rangeBinMigration = \
     np.exp(1j*2*np.pi*chirpSlope*(2*objectVelocity_mps/lightSpeed)*interRampTime*adcSamplingTime*np.arange(numSamples)[:,None]*np.arange(numChirps)[None,:])
 
-radarSignal = rangeSignal[:,None] * dopplerSignal[None,:] * rangeBinMigrationTerm
+radarSignal = rangeSignal[:,None] * dopplerSignal[None,:] * rangeBinMigration
 
 noise = (sigma/np.sqrt(2))*np.random.randn(numSamples*numChirps) + 1j*(sigma/np.sqrt(2))*np.random.randn(numSamples*numChirps)
 noise = noise.reshape(numSamples, numChirps)
@@ -98,6 +113,11 @@ plt.axvline(objectRange_m, color = 'k')
 plt.xlabel('Range (m)')
 plt.ylabel('dBm')
 plt.grid(True)
+
+
+
+
+
 
 
 