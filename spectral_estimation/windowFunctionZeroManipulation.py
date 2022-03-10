52509a5a4edc534562089c8720bc3a97dea64b9a
Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>
2022-03-10 12:44:31 +0530

    Synthesize a pseudo inverse based response by manipulating the zeros of a window function
    In this commit, I have developed a script which synthesizes a pseudo inverse like response(of Vandermonde matrix) by manipulating the zeros of a pre-defined simple function. In this case, we have used a kaiser window to start with. We treat the window function as a system finite impulse response(FIR) and find the zeros of the window function. We then move the system zeros to the position of the interferer frequency(digital) locations. In fact, the zeros have to be pushed to the negative of the interferer locations. This is because, we need to synthesize a vandermonde pseudo inverse like response which has an inherent conjugation and hence the frequency response is also flipped. So we need to place the zeros at the negative of the interferer locations. For this, we need to identify the system zeros closest to the interferer tones and this closeness measure has to be done modulo 2pi. For example, lets say 2 of the system zeros to begin with are located at 0 deg and 330 degrees. Now say there is an interferer at 358 degrees. Then, if we were to just take the smallest magnitude difference of the phases, then the interferer would seem closer to 330 degress than 0 degrees since (358-330 < 358-0). But in reality, 358 degrees or -2 degrees is closer to 0. So in order to take care of such cases, we use a modulo difference to find the closest zeros to the interferer tones(actually negative of the interferer tones) and replace the closest system zeros with the negative of the interferer. Once the zeros are manipulated, we again reconstruct the impulse response using the zpk2tf function from the scipy.signal library. In this script, I have taken a simple case of 2 tones. Hence we get two impulse responses. For the first impulse response, the second tone is the interferer and it is nulled out with the nearest zero and for the second impulse response, the first tone becomes the interferer. I have plotted the frequency response of the synthesised impulse responses and overlaid them with the vandermonde pseudo inverse response. The location of the nulls/zeros is matching expectation. One thing I'm circumspect about is the synthesised impulse response correlation with the observaion to get the complex coefficients. The absolute values will not match the least squares based solution but will the structure of the phase across MIMO channels be maintained? I will need to simulate to test this aspect.
    
    In the next commit, I will replace the zpk2tf function with my own function which does an iterative convolution treating the overall reponse as a series of cascaded FIR filters each with a single zero.
    
    Note: An FIR system with N taps, will have N-1 zeros.
    
    Signed-off-by: Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>

---
+++ spectral_estimation/windowFunctionZeroManipulation.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  8 11:53:53 2022
+
+@author: saiguna
+"""
+
+
+import numpy as np
+import scipy.signal as sig
+import matplotlib.pyplot as plt
+
+def angular_distance(theta_1, theta_2, mod=2*np.pi):
+    difference = np.abs(theta_1[:,None] % mod - theta_2[None,:] % mod)
+    return np.minimum(difference, mod - difference)
+
+
+plt.close('all')
+
+numSamp = 14
+numFFT = 1024
+# win_coeff = np.hamming(numSamp)
+win_coeff = np.kaiser(numSamp,beta=4.5)
+
+num = win_coeff
+denom = 1
+zeros_window,p,k = sig.tf2zpk(num,denom)
+
+
+zeroMag = np.abs(zeros_window).astype(np.float32)
+ZerosNotOnUnitCircle = np.where(zeroMag!=1.0)[0]
+
+winfft = np.fft.fft(win_coeff,n=numFFT)
+freAxis_bipolar = np.arange(-numFFT//2,numFFT//2)*2*np.pi/numFFT
+winfftshift = np.fft.fftshift(winfft)
+
+plt.figure(1,figsize=(20,10))
+plt.subplot(1,2,1)
+plt.title('Kaiser window spectrum')
+plt.plot(freAxis_bipolar, 20*np.log10(np.abs(winfftshift)));plt.grid(True)
+plt.xlabel('rad/samp')
+for i in range(numSamp-1):
+    plt.axvline(np.angle(zeros_window[i]),c='m',lw=2,alpha=0.4)
+plt.grid(True)
+
+x = np.cos(np.linspace(0,2*np.pi,500))
+y = np.sin(np.linspace(0,2*np.pi,500))
+plt.subplot(1,2,2)
+plt.title('Zero plot for Kaiser window')
+plt.plot(zeros_window.real, zeros_window.imag, 'o', fillstyle='none', ms=14)
+plt.plot(x,y,color='k');
+plt.axis('equal')
+plt.grid(True)
+plt.xlabel('Real')
+plt.xlabel('Imag')
+
+
+numOrigSamp = 168
+bin1 = 10 + np.random.uniform(-0.5,0.5,1)[0] # bin on a scale of numOrigSamp. Choose from [-numOrigSamp/2, numOrigSamp/2]
+bin2 = 13 + np.random.uniform(-0.5,0.5,1)[0]
+
+bins = np.array([bin1,bin2])
+digFreq = bins*2*np.pi/numOrigSamp
+phasor = np.exp(-1j*digFreq) # This negative is required since the pseudo inverse has a response conjugate to the true response
+
+phase_window = np.angle(zeros_window)
+# phase_window[phase_window<0] += 2*np.pi # Convert digital frequencies from [-pi,pi] scale to [0,2*np.pi] scale
+
+""" Push the window zeros to the place of interferers"""
+# Filter coefficients for signal 1
+signalFreq = digFreq[0]
+interfererFreq = -1*np.array([digFreq[1]])
+ind_closest_winZeros = np.argmin(angular_distance(interfererFreq,phase_window),axis=1)
+
+phase_window_manipulated = phase_window.copy()
+phase_window_manipulated[ind_closest_winZeros] = interfererFreq
+zeros_sig1 = np.exp(1j*phase_window_manipulated)
+
+sig1_tf, _ = sig.zpk2tf(zeros_sig1,[],1)
+
+sig1_tf_fft = np.fft.fft(sig1_tf, n=numFFT)
+sig1_tf_fft = np.fft.fftshift(sig1_tf_fft)
+
+
+# Filter coefficients for signal 2
+signalFreq = digFreq[1]
+interfererFreq = -1*np.array([digFreq[0]]) # Need to place the zeros at the negative of the interferer locations.
+ind_closest_winZeros = np.argmin(angular_distance(interfererFreq,phase_window),axis=1)
+
+phase_window_manipulated = phase_window.copy()
+phase_window_manipulated[ind_closest_winZeros] = interfererFreq
+zeros_sig2 = np.exp(1j*phase_window_manipulated)
+
+sig2_tf, _ = sig.zpk2tf(zeros_sig2,[],1)
+
+sig2_tf_fft = np.fft.fft(sig2_tf, n=numFFT)
+sig2_tf_fft = np.fft.fftshift(sig2_tf_fft)
+
+vandermondeMatrix = np.exp(1j*digFreq[None,:]*np.arange(numSamp)[:,None])
+vandermondeMatrixInv = np.linalg.pinv(vandermondeMatrix)
+vandermondeMatrixInv_fft = np.fft.fft(vandermondeMatrixInv,axis=1, n=numFFT)
+vandermondeMatrixInv_fft = np.fft.fftshift(vandermondeMatrixInv_fft,axes=1)
+
+z_vandInv_row1, p_vanInv_row1, k = sig.tf2zpk(vandermondeMatrixInv[0,:],1)
+z_vandInv_row2, p_vanInv_row2, k = sig.tf2zpk(vandermondeMatrixInv[1,:],1)
+
+
+plt.figure(2,figsize=(20,10))
+plt.suptitle('Frequency Response of Vandermonde pseudo inverse')
+plt.subplot(2,2,1)
+plt.title('Frequency Response of 1st row of pseudo inverse matrix')
+plt.plot(freAxis_bipolar, 20*np.log10(np.abs(vandermondeMatrixInv_fft[0,:])), color='k', label='Inv Vand Mat 1st row spectrum')
+plt.axvline(-digFreq[0] ,color='b', label='signal')
+plt.axvline(-digFreq[1] , color='r', label='interferer')
+plt.plot(freAxis_bipolar, 20*np.log10(np.abs(sig1_tf_fft)), label='synthesized response for sig1')
+plt.xlabel('Dig Freq (rad/samp)')
+plt.grid(True)
+plt.legend()
+
+plt.subplot(2,2,2)
+plt.title('Zeros of 1st row of pseudo inverse matrix')
+plt.plot(z_vandInv_row1.real, z_vandInv_row1.imag, 'o', fillstyle='none', ms=14, label='Inv Vand Mat 1st row zeros')
+plt.plot(phasor[0].real, phasor[0].imag, 'd', fillstyle='none', ms=14, color='b', label='signal')
+plt.plot(phasor[1].real, phasor[1].imag, 's', fillstyle='none', ms=14, color='r', label='interferer')
+plt.plot(zeros_sig1.real, zeros_sig1.imag, '+', fillstyle='none', ms=14, color='k', label='zeros synthesized response for sig1')
+plt.plot(x,y,color='k');
+plt.axis('equal')
+plt.grid(True)
+plt.xlabel('Real')
+plt.xlabel('Imag')
+plt.legend()
+
+
+plt.subplot(2,2,3)
+plt.title('Frequency Response of 2nd row of pseudo inverse matrix')
+plt.plot(freAxis_bipolar, 20*np.log10(np.abs(vandermondeMatrixInv_fft[1,:])), color='k', label='Inv Vand Mat 1st row spectrum')
+plt.axvline(-digFreq[1] , color='b', label='signal')
+plt.axvline(-digFreq[0] ,color='r', label='interferer')
+plt.plot(freAxis_bipolar, 20*np.log10(np.abs(sig2_tf_fft)), label='synthesized response for sig2')
+plt.xlabel('Dig Freq (rad/samp)')
+plt.grid(True)
+plt.legend()
+
+plt.subplot(2,2,4)
+plt.title('Zeros of 2nd row of pseudo inverse matrix')
+plt.plot(z_vandInv_row2.real, z_vandInv_row2.imag, 'o', fillstyle='none', ms=14, label='Inv Vand Mat 2nd row zeros')
+plt.plot(phasor[1].real, phasor[1].imag, 'd', fillstyle='none', ms=14, color='b', label='signal')
+plt.plot(phasor[0].real, phasor[0].imag, 's', fillstyle='none', ms=14, color='r', label='interferer')
+plt.plot(zeros_sig2.real, zeros_sig2.imag, '+', fillstyle='none', ms=14, color='k', label='zeros synthesized response for sig2')
+plt.plot(x,y,color='k');
+plt.axis('equal')
+plt.grid(True)
+plt.xlabel('Real')
+plt.xlabel('Imag')
+plt.legend()
