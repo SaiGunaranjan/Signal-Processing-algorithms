54767ebae44eb06e57ff6c024ef007f68a2dccfe
Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>
2022-03-10 11:24:53 +0530

    Plot and analyse response of Inverse Vandermonde Matrix on the bipolar frequency axis scale from -pi to pi
    In this commit, I analyse the frequency response/zeros of the Inverse Vandermonde rows on a bipolar frequency grid i.e from -pi to +pi. In the previous commit, the analysis was being done from 0 to 2pi. I have also added some useful comments to explain some important lines of code. The following are the other changes made in this commit.
    1. Added a comment explaining why we take negative of the phases for the signal frequencies
    2. Applying an FFTshift on the FFT of the Inverse of the vandermonde matrix. This is so we cna work on the bipolar freqeuncy axis from -pi to +pi
    3. Changed the plotting color and style.
    
    Signed-off-by: Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>

--- spectral_estimation/FIR_interpretation_VandermondePseudoInverse.py
+++ spectral_estimation/FIR_interpretation_VandermondePseudoInverse.py
@@ -20,30 +20,32 @@ numFFT = 1024
 x = np.cos(np.linspace(0,2*np.pi,500))
 y = np.sin(np.linspace(0,2*np.pi,500))
 
-bin1 = 10 + np.random.uniform(-0.5,0.5,1)[0] # bin on a scale of numOrigSamp
+bin1 = 10 + np.random.uniform(-0.5,0.5,1)[0] # bin on a scale of numOrigSamp. Choose from [-numOrigSamp/2, numOrigSamp/2]
 bin2 = 13 + np.random.uniform(-0.5,0.5,1)[0]
 
 bins = np.array([bin1,bin2])
 digFreq = bins*2*np.pi/numOrigSamp
 vandermondeMatrix = np.exp(1j*digFreq[None,:]*np.arange(numSamp)[:,None])
 
-phasor = np.exp(-1j*digFreq)
+phasor = np.exp(-1j*digFreq) # This negative is required since the pseudo inverse has a response conjugate to the true response
 
 vandermondeMatrixInv = np.linalg.pinv(vandermondeMatrix)
 vandermondeMatrixInv_fft = np.fft.fft(vandermondeMatrixInv,axis=1, n=numFFT)
+vandermondeMatrixInv_fft = np.fft.fftshift(vandermondeMatrixInv_fft,axes=1)
 
 z_vandInv_row1, p_vanInv_row1, k = sig.tf2zpk(vandermondeMatrixInv[0,:],1)
 z_vandInv_row2, p_vanInv_row2, k = sig.tf2zpk(vandermondeMatrixInv[1,:],1)
 
 freAxis_unipolar = np.arange(0,numFFT)*2*np.pi/numFFT
+freAxis_bipolar = np.arange(-numFFT//2,numFFT//2)*2*np.pi/numFFT
 
 plt.figure(2,figsize=(20,10))
 plt.suptitle('Frequency Response of Vandermonde pseudo inverse')
 plt.subplot(2,2,1)
 plt.title('Frequency Response of 1st row of pseudo inverse matrix')
-plt.plot(freAxis_unipolar, 20*np.log10(np.abs(vandermondeMatrixInv_fft[0,:])), label='Inv Vand Mat 1st row spectrum')
-plt.axvline(-digFreq[0] + 2*np.pi,color='orange', label='signal')
-plt.axvline(-digFreq[1] + 2*np.pi, color='m', label='interferer')
+plt.plot(freAxis_bipolar, 20*np.log10(np.abs(vandermondeMatrixInv_fft[0,:])), color='k', label='Inv Vand Mat 1st row spectrum')
+plt.axvline(-digFreq[0] ,color='b', label='signal')
+plt.axvline(-digFreq[1] , color='r', label='interferer')
 plt.xlabel('Dig Freq (rad/samp)')
 plt.grid(True)
 plt.legend()
@@ -51,8 +53,8 @@ plt.legend()
 plt.subplot(2,2,2)
 plt.title('Zeros of 1st row of pseudo inverse matrix')
 plt.plot(z_vandInv_row1.real, z_vandInv_row1.imag, 'o', fillstyle='none', ms=14, label='Inv Vand Mat 1st row zeros')
-plt.plot(phasor[0].real, phasor[0].imag, 'd', fillstyle='none', ms=14, color='orange', label='signal')
-plt.plot(phasor[1].real, phasor[1].imag, 's', fillstyle='none', ms=14, color='m', label='interferer')
+plt.plot(phasor[0].real, phasor[0].imag, 'd', fillstyle='none', ms=14, color='b', label='signal')
+plt.plot(phasor[1].real, phasor[1].imag, 's', fillstyle='none', ms=14, color='r', label='interferer')
 plt.plot(x,y,color='k');
 plt.axis('equal')
 plt.grid(True)
@@ -63,9 +65,9 @@ plt.legend()
 
 plt.subplot(2,2,3)
 plt.title('Frequency Response of 2nd row pseudo inverse matrix')
-plt.plot(freAxis_unipolar, 20*np.log10(np.abs(vandermondeMatrixInv_fft[1,:])), label='Inv Vand Mat 1st row spectrum')
-plt.axvline(-digFreq[1] + 2*np.pi, color='orange', label='signal')
-plt.axvline(-digFreq[0] + 2*np.pi,color='magenta', label='interferer')
+plt.plot(freAxis_bipolar, 20*np.log10(np.abs(vandermondeMatrixInv_fft[1,:])), color='k', label='Inv Vand Mat 1st row spectrum')
+plt.axvline(-digFreq[1] , color='b', label='signal')
+plt.axvline(-digFreq[0] ,color='r', label='interferer')
 plt.xlabel('Dig Freq (rad/samp)')
 plt.grid(True)
 plt.legend()
@@ -73,8 +75,8 @@ plt.legend()
 plt.subplot(2,2,4)
 plt.title('Zeros of 2nd row of pseudo inverse matrix')
 plt.plot(z_vandInv_row2.real, z_vandInv_row2.imag, 'o', fillstyle='none', ms=14, label='Inv Vand Mat 2nd row zeros')
-plt.plot(phasor[1].real, phasor[1].imag, 'd', fillstyle='none', ms=14, color='orange', label='signal')
-plt.plot(phasor[0].real, phasor[0].imag, 's', fillstyle='none', ms=14, color='m', label='interferer')
+plt.plot(phasor[1].real, phasor[1].imag, 'd', fillstyle='none', ms=14, color='b', label='signal')
+plt.plot(phasor[0].real, phasor[0].imag, 's', fillstyle='none', ms=14, color='r', label='interferer')
 plt.plot(x,y,color='k');
 plt.axis('equal')
 plt.grid(True)
