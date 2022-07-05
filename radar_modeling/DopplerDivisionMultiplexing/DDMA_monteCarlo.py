2de273bf484105906410608183091970844d694a
Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>
2022-07-05 11:12:52 +0530

    Run MonteCarlo on DDMA architecture
    In this commit, I have written a script to run a Monte Carlo siulation
    run to check and assess the performance of the DDMA scheme. The idea is
    obtain curves showing the Angle error vs range SNR for different number
    of DDMA ramps. IN each Monte carlo run, I randomize the number of
    Dopplers in a given range from 3 to 9 so effectively the number of
    components in the DOppler spectrum will be 12 to 36 (since 4 Txs). This
    is to model the Steradian RADAR platforms which include eithere 12
    TX/13Tx DDMA. So if there is a single Doppler in a given range bin, then
    the Doppler spectrum would consist of 12 components. Since in my model,
    the number of simultaneous Txs is 4 so I choose a minimum of 3 Dopplers
    to obtain the 12 components as in Steradians RADAR plotform. Similarly,
    the max Dopplers in a range bin can be assumed to be about 3. In which
    case, Steradian's platform would consist of 12x3 = 36 Doppler
    components. So, in order to achieve 36 Doppler components i my model, I
    need 9 Dopplers in a range bin. On running the Monte carlo simulation, I
    observed that the angle error is about 5 degrees even for very good
    SNRs. I need to debug why this is happening.
    
    Signed-off-by: Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>

---
+++ radar_modeling/DopplerDivisionMultiplexing/DDMA_monteCarlo.py
@@ -0,0 +1,304 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  3 16:36:29 2021
+
+@author: saiguna
+"""
+
+""" In this script, I have modeled how the phase shifter error(from the
+ground truth)for each unique phase shifter manifests as periodic repetetions
+when the phase is swept over several ramps. This periodic component for each
+of the phase shifter manifests as spurs in the Doppler spectrum. These spurs get
+aggravated when the phase delta(in degrees) programmed per ramp divides 360 degress
+exactly. In such case, the same phase shifter code is applied periodically
+at every 360/phase_delta ramp number. For example, if the phase delta per ramp
+is chosen as 30 degrees, then 360/30 = 12 i.e. the starting ramp and the 12th ramp
+will have the same phase shifter applied. Similarly, the 2nd ramp and the 13th ramp
+have the same phase shifter applied and so on. Now since each phase shifter
+has its own "signature"(error in terms of DNL wrt ground truth), there will be
+a periodic pattern every 12th ramp. To break this periodicity, I have chosen
+a phase step which doesn't divide 360 degrees. For example, if the phase step per ramp
+is 29 degress instead of 30 degress, then at the 13th ramp, the phase is 29*13 = 377 = 17 degress.
+But this is not equal to 0 degrees i.e the phase shifter for the 13th ramp
+is not the same as the phase shifter for the 1st ramp. Similarly, the second ramp
+has a phase of 29 degress while the 14th ramp has a phase of 29*14= 406 = 46 degress which
+is not the same as 29 degress. Hence the phase shifter for the 14th ramp
+is not the same as the phase shifter for the 2nd ramp. Hence just by making
+the phase change per ramp not a divisor of 360 degress, we remove the periodicity.
+We also observe that the noise floor increases by 3 dB for each Tx that is added
+to the simultaneous transmission i.e., when we move from 3 Tx to 4 Tx, the noise floor raises by another 3 dB.
+This is not very clear to me and I need to understand this better!!
+"""
+
+""" In this commit, I have also modeled the Txs/Rxs with simulataneous transmission from all Txs
+each with its own phase code per ramp and have also been able to estimate MIMO coeficients.
+I have assumed 4 Txs each separated by 2mm (lamda/2) and 8 Rxs each separated by 8mm (2lamda).
+Also added Doppler to the object"""
+
+""" The derivation for the DDMA scheme is available in the below location:
+    https://saigunaranjan.atlassian.net/wiki/spaces/RM/pages/1966081/Code+Division+Multiple+Access+in+FMCW+RADAR"""
+
+
+
+""" To verify if the DDMA modelling has been done correctly and the DDMA is infact a special case of ABF
+    make the following changes
+    1. Set  phaseStepPerTx_deg = 0
+    2. Set objectAzAngle_deg = 0 since ABF holds for object at boresight
+    3. Make the binSNR a reasonable number say 10 so that the tone sticks out to begin with in the range spectrum
+    4. Make numTx_simult vary from 1, 2, 3, 4 to check if the SNR of the target at boresight scales by 6, 9, 12 dB
+    for numTx_simult = 2, 3, 4 respectively"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import time as time
+
+tstart = time.time()
+
+plt.close('all')
+
+
+flagRBM = 1
+if (flagRBM == 1):
+    print('\n\nRange Bin Migration term has been enabled\n\n')
+
+numTx_simult = 4
+numRx = 8
+numMIMO = numTx_simult*numRx
+numSamp = 2048 # Number of ADC time domain samples
+numSampPostRfft = numSamp//2
+numAngleFFT = 2048
+txSpacing = 2e-3
+rxSpacing = 4*txSpacing
+lightSpeed = 3e8
+c = 3e8
+numBitsPhaseShifter = 5
+numPhaseCodes = 2**numBitsPhaseShifter
+DNL = 360/(numPhaseCodes) # DNL in degrees
+
+""" Chirp Parameters"""
+
+numDoppFFT = 2048
+chirpBW = 1e9 # Hz
+centerFreq = 76.5e9 # GHz
+interRampTime = 44e-6 # us
+rampSamplingRate = 1/interRampTime
+rangeRes = c/(2*chirpBW)
+maxRange = numSampPostRfft*rangeRes # m
+lamda = lightSpeed/centerFreq
+""" With 30 deg, we see periodicity since 30 divides 360 but with say 29 deg, it doesn't divide 360 and hence periodicity is significantly reduced"""
+phaseStepPerTx_deg = 29#29.3
+
+Fs_spatial = lamda/txSpacing
+angAxis_deg = np.arcsin(np.arange(-numAngleFFT//2, numAngleFFT//2)*(Fs_spatial/numAngleFFT))*180/np.pi
+
+""" Phase Shifter settings"""
+phaseStepPerRamp_deg = np.arange(numTx_simult)*phaseStepPerTx_deg # Phase step per ramp per Tx
+phaseStepPerRamp_rad = (phaseStepPerRamp_deg/360)*2*np.pi
+phaseShifterCodes = DNL*np.arange(numPhaseCodes)
+phaseShifterNoise = np.random.uniform(-DNL/2, DNL/2, numPhaseCodes)
+phaseShifterCodes_withNoise = phaseShifterCodes + phaseShifterNoise
+
+
+""" RF parameters """
+thermalNoise = -174 # dBm/Hz
+noiseFigure = 10 # dB
+baseBandgain = 34 #dB
+adcSamplingRate = 56.25e6 # 56.25 MHz
+adcSamplingTime = 1/adcSamplingRate # s
+chirpOnTime = numSamp*adcSamplingTime
+chirpSlope = chirpBW/chirpOnTime
+dBFs_to_dBm = 10
+totalNoisePower_dBm = thermalNoise + noiseFigure + baseBandgain + 10*np.log10(adcSamplingRate)
+totalNoisePower_dBFs = totalNoisePower_dBm - 10
+noiseFloor_perBin = totalNoisePower_dBFs - 10*np.log10(numSamp) # dBFs/bin
+noisePower_perBin = 10**(noiseFloor_perBin/10)
+totalNoisePower = noisePower_perBin*numSamp # sigmasquare totalNoisePower
+sigma = np.sqrt(totalNoisePower)
+
+""" Derived Parameters """
+# snrGainDDMA = 10*np.log10(numTx_simult**2) #dB
+# snrGainDopplerFFT = 10*np.log10(numRamps) #dB
+# totalsnrGain = snrGainDDMA + snrGainDopplerFFT
+# print('Total SNR gain ( {0:.0f} Tx DDMA + {1:.0f} point Doppler FFT) = {2:.2f} dB'.format(numTx_simult, numRamps, totalsnrGain))
+
+chirpSamplingRate = 1/interRampTime
+maxVelBaseband_mps = (chirpSamplingRate/2) * (lamda/2) # m/s
+FsEquivalentVelocity = 2*maxVelBaseband_mps # Fs = 2*Fs/2
+
+""" MonteCarlo Parameters"""
+numChirpsDDMA = np.arange(50,170,20) #np.arange(50,170,40) #np.arange(50,170,20) # np.array([140])
+range_binSNRArray = np.arange(-20, 30, 2) #np.arange(-20, 30, 4) # np.arange(-20, 30, 2) # dB
+numMonteCarloRuns = 100#50 #100 # 1
+numChirpsMC = len(numChirpsDDMA)
+numSnrMC = len(range_binSNRArray)
+angleErrorMatrix = np.zeros((numChirpsMC, numSnrMC))
+count_rampMC = 0
+
+
+for numRamps in numChirpsDDMA:
+    # numRamps = 140
+    rampPhaseIdeal_deg = phaseStepPerRamp_deg[:,None]*(np.arange(numRamps)[None,:])
+    rampPhaseIdeal_degWrapped = np.mod(rampPhaseIdeal_deg, 360)
+    phaseCodesIndexToBeApplied = np.argmin(np.abs(rampPhaseIdeal_degWrapped[:,:,None] - phaseShifterCodes_withNoise[None,None,:]),axis=2)
+    phaseCodesToBeApplied = phaseShifterCodes_withNoise[phaseCodesIndexToBeApplied]
+    phaseCodesToBeApplied_rad = (phaseCodesToBeApplied/180) * np.pi
+    velocityRes = (chirpSamplingRate/numRamps) * (lamda/2)
+    count_snrMC = 0
+
+    for binSNR in range_binSNRArray:
+        tstart_snr = time.time()
+        # binSNR = -3 # dB
+        signalPowerdBFs = noiseFloor_perBin + binSNR
+        signalPower = 10**(signalPowerdBFs/10)
+        signalAmplitude = np.sqrt(signalPower)
+        signalPhase = np.exp(1j*np.random.uniform(-np.pi, np.pi))
+        signalphasor = signalAmplitude*signalPhase
+        errorAngArray = np.empty([0])
+        for iter_num in np.arange(numMonteCarloRuns):
+            """ Target definition"""
+            numDopUniqRbin = int(np.random.randint(low=3, high=9, size=1)) #2 # Number of Dopplers in a given range bin
+            objectRange = np.random.uniform(10,maxRange-10) # 60.3 # m
+            objectVelocity_mps = np.random.uniform(-maxVelBaseband_mps-2*FsEquivalentVelocity, \
+                                                    maxVelBaseband_mps+2*FsEquivalentVelocity, numDopUniqRbin)  #np.array([-10,-10.1]) #np.array([-10,23])# m/s
+            # print('Velocities (mps):', np.round(objectVelocity_mps,2))
+            objectAzAngle_deg = np.random.uniform(-50,50, numDopUniqRbin) #np.array([30,-10])
+            objectAzAngle_rad = (objectAzAngle_deg/360) * (2*np.pi)
+
+            objectVelocity_baseBand_mps = np.mod(objectVelocity_mps, FsEquivalentVelocity) # modulo Fs [from 0 to Fs]
+            objectVelocityBin = objectVelocity_baseBand_mps/velocityRes
+            objectRangeBin = objectRange/rangeRes
+            if (flagRBM == 1):
+                rangeMoved = objectRange + objectVelocity_mps[:,None]*interRampTime*np.arange(numRamps)[None,:]
+            else:
+                rangeMoved = objectRange + 0*objectVelocity_mps[:,None]*interRampTime*np.arange(numRamps)[None,:]
+
+            rangeBinsMoved = np.floor(rangeMoved/rangeRes).astype('int32')
+
+
+            rangeTerm = signalphasor*np.exp(1j*((2*np.pi*objectRangeBin)/numSamp)*np.arange(numSamp))
+            dopplerTerm = np.exp(1j*((2*np.pi*objectVelocityBin[:,None])/numRamps)*np.arange(numRamps)[None,:]) # [number of Dopplers/range, numRamps]
+            """ Range Bin migration term"""
+            rangeBinMigration = \
+                np.exp(1j*2*np.pi*chirpSlope*(2*objectVelocity_mps[:,None,None]/lightSpeed)*interRampTime*adcSamplingTime*np.arange(numRamps)[None,:,None]*np.arange(numSamp)[None,None, :])
+
+
+            rxSignal = np.exp(1j*(2*np.pi/lamda)*rxSpacing*np.sin(objectAzAngle_rad[:,None])*np.arange(numRx)[None,:]) # [number of Angles/RD, numRx]
+            txSignal = np.exp(1j*(2*np.pi/lamda)*txSpacing*np.sin(objectAzAngle_rad[:,None])*np.arange(numTx_simult)[None,:]) # [number of Angles/RD, numTx]
+            signal_phaseCode = np.exp(1j*phaseCodesToBeApplied_rad)
+            phaseCodedTxSignal = dopplerTerm[:,None,:] * signal_phaseCode[None,:,:] * txSignal[:,:,None] # [numDopp, numTx, numRamps]
+            phaseCodedTxRxSignal = phaseCodedTxSignal[:,:,:,None]*rxSignal[:,None,None,:] #[numDopp, numTx, numRamps, numTx, numRx]
+            phaseCodedTxRxSignal_withRangeTerm = rangeTerm[None,None,None,None,:] * phaseCodedTxRxSignal[:,:,:,:,None]
+            if (flagRBM == 1):
+                phaseCodedTxRxSignal_withRangeTerm = phaseCodedTxRxSignal_withRangeTerm * rangeBinMigration[:,None,:,None,:]
+            signal = np.sum(phaseCodedTxRxSignal_withRangeTerm, axis=(0,1)) # [numRamps,numRx, numSamp]
+
+
+            noise = (sigma/np.sqrt(2))*np.random.randn(numRamps*numRx*numSamp) + 1j*(sigma/np.sqrt(2))*np.random.randn(numRamps*numRx*numSamp)
+            noise = noise.reshape(numRamps, numRx, numSamp)
+            signal = signal + noise
+
+            signal_rangeWin = signal*np.hanning(numSamp)[None,None,:]
+            signal_rfft = np.fft.fft(signal_rangeWin,axis=2)/numSamp
+            signal_rfft = signal_rfft[:,:,0:numSampPostRfft]
+            signal_rfft_powermean = np.mean(np.abs(signal_rfft)**2,axis=(0,1))
+
+            rangeBinsToSample = rangeBinsMoved
+            chirpSamp_givenRangeBin = signal_rfft[np.arange(numRamps)[None,:],:,rangeBinsToSample]
+
+            if (flagRBM == 1):
+                """ Correcting for the Pi phase jump caused due to the Range bin Migration"""
+                binDelta = np.abs(rangeBinsToSample[:,1::] - rangeBinsToSample[:,0:-1])
+                tempVar = binDelta*np.pi
+                binMigrationPhaseCorrTerm = np.zeros((rangeBinsToSample.shape),dtype=np.float32)
+                binMigrationPhaseCorrTerm[:,1::] = tempVar
+                binMigrationPhasorCorrTerm = np.exp(-1j*np.cumsum(binMigrationPhaseCorrTerm,axis=1))
+                chirpSamp_givenRangeBin = chirpSamp_givenRangeBin*binMigrationPhasorCorrTerm[:,:,None]
+
+                """ Correcting for the Doppler modulation caused due to the Range bin Migration"""
+                rbmModulationAnalogFreq = (chirpBW/lightSpeed)*objectVelocity_mps
+                dopplerBinOffset_rbm = (rbmModulationAnalogFreq/rampSamplingRate)*numDoppFFT
+                # rbmModulationDigitalFreq = (rbmModulationAnalogFreq/rampSamplingRate)*2*np.pi
+                # rbmModulationCorrectionTerm = np.exp(-1j*rbmModulationDigitalFreq[:,None]*np.arange(numRamps)[None,:])
+                # chirpSamp_givenRangeBin = chirpSamp_givenRangeBin*rbmModulationCorrectionTerm[:,:,None]
+            else:
+                dopplerBinOffset_rbm = np.zeros((numDopUniqRbin,))
+
+
+            signalWindowed = chirpSamp_givenRangeBin*np.hanning(numRamps)[None,:,None]
+            signalFFT = np.fft.fft(signalWindowed, axis=1, n = numDoppFFT)/numRamps
+            signalFFTShift = signalFFT #np.fft.fftshift(signalFFT, axes= (0,))
+            signalFFTShiftSpectrum = np.abs(signalFFTShift)**2
+            signalFFTShiftSpectrum = signalFFTShiftSpectrum/np.amax(signalFFTShiftSpectrum, axis=1)[:,None,:] # Normalize the spectrum for each Rx
+            signalMagSpectrum = 10*np.log10(np.abs(signalFFTShiftSpectrum))
+
+            objectVelocityBinNewScale = (objectVelocityBin/numRamps)*numDoppFFT
+            binOffset_Txphase = (phaseStepPerRamp_rad/(2*np.pi))*numDoppFFT
+            dopplerBinsToSample = np.round(objectVelocityBinNewScale[:,None] + dopplerBinOffset_rbm[:,None] + binOffset_Txphase[None,:]).astype('int32')
+            dopplerBinsToSample = np.mod(dopplerBinsToSample, numDoppFFT)
+
+
+
+            # plt.figure(2, figsize=(20,10))
+            # plt.suptitle('Doppler Spectrum with ' + str(numTx_simult) + 'Txs simultaneously ON in CDM')
+            # for ele in range(numDopUniqRbin):
+            #     plt.subplot(np.floor_divide(numDopUniqRbin-1,3)+1,min(3,numDopUniqRbin),ele+1)
+            #     plt.plot(signalMagSpectrum[ele,:,0].T, lw=2, label='Target speed = ' + str(np.round(objectVelocity_mps[ele],2)) + ' mps') # Plotting only the 0th Rx instead of all 8
+            #     plt.vlines(dopplerBinsToSample[ele,:] ,ymin = -70, ymax = 10)
+            #     plt.xlabel('Doppler Bins')
+            #     plt.ylabel('Power dBFs')
+            #     plt.grid(True)
+            #     plt.legend()
+
+
+            mimoCoefficients_eachDoppler_givenRange = signalFFT[np.arange(numDopUniqRbin)[:,None],dopplerBinsToSample,:] # numTx*numDopp x numRx
+            mimoCoefficients_flatten = np.transpose(mimoCoefficients_eachDoppler_givenRange,(0,2,1)).reshape(-1,numTx_simult*numRx)
+            mimoCoefficients_flatten = mimoCoefficients_flatten*np.hanning(numMIMO)[None,:]
+            ULA_spectrum = np.fft.fft(mimoCoefficients_flatten,axis=1,n=numAngleFFT)/(numMIMO)
+            ULA_spectrum = np.fft.fftshift(ULA_spectrum,axes=(1,))
+            ULA_spectrumMag = np.abs(ULA_spectrum)
+            angInd = np.argmax(ULA_spectrumMag,axis=1)
+            estAngDeg = angAxis_deg[angInd]
+            errorAng = objectAzAngle_deg - estAngDeg
+            errorAngArray = np.hstack((errorAngArray,errorAng))
+
+            # plt.figure(4, figsize=(20,10))
+            # plt.suptitle('MIMO ULA Angle spectrum')
+            # for ele in range(numDopUniqRbin):
+            #     plt.subplot(np.floor_divide(numDopUniqRbin-1,3)+1,min(3,numDopUniqRbin),ele+1)
+            #     plt.plot(angAxis_deg, 20*np.log10(np.abs(ULA_spectrum[ele,:])),lw=2)
+            #     plt.vlines(objectAzAngle_deg[ele], ymin = -170, ymax = -110)
+            #     plt.xlabel('Angle (deg)')
+            #     plt.ylabel('dB')
+            #     plt.grid(True)
+
+        angErrorStd = np.std(errorAngArray)
+        angleErrorMatrix[count_rampMC,count_snrMC] = angErrorStd
+
+        # print('Monte Carlo completed for {}/{} ramp steps, {}/{} SNR steps'.format(count_rampMC, numChirpsMC, count_snrMC, numSnrMC))
+
+        count_snrMC += 1
+        tstop_snr = time.time()
+        timeSNR = tstop_snr - tstart_snr
+        print('Time taken for {0}/{1} ramp steps, {2}/{3} SNR run = {4:.2f} s'.format(count_rampMC, numChirpsMC, count_snrMC, numSnrMC, timeSNR))
+    count_rampMC +=1
+    print('\n\n')
+
+tstop = time.time()
+
+timeMC = tstop - tstart
+print('Total time for Monte-Carlo run = {0:.2f} min'.format(timeMC/60))
+
+legend_list = [str(x) + ' ramps' for x in numChirpsDDMA]
+plt.figure(1,figsize=(20,10), dpi=200)
+plt.title('Angle Error(std) vs SNR')
+plt.plot(range_binSNRArray, angleErrorMatrix.T, '-o')
+plt.xlabel('SNR (dB)')
+plt.ylabel('Angle Error std (deg)')
+plt.grid(True)
+plt.legend(legend_list)
+
+
+
+
+
+
