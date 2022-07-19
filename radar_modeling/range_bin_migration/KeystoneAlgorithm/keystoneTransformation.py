a2e2356552ec5c3d1d2b76c7fa6d2375b9b39cf1
Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>
2022-07-19 17:52:51 +0530

    Keystone transformation to fix range bin migration
    Keystone transformation is an algorithm to fix the range bin migration in the detection segment where a single Tx transmits several chirps. In this commit, I have coded the Keystone transformation for an FMCW RADAR architecture. The reference paaper I have used is given as a link in the script. The keystone algorithm works on the raw ADC time samples and not on the range FFT data. The principle behind the keystone algorithm is that, if there is no range bin migration, then the phase variation across the chirp samples is the same no matter what ADC sample we choose. In other words, the phase variation across the ramps for the 100th ADC sample should be the same as the phase variation for the 1012 sample. This is because there will only be two terms namely the range term and the Doppler term and both of these are completely de-coupled.  The initial phase though will be different (because this manifests as range frequency) but the phase function/variation has to be the same. However, when there is a range bin migration across chirps (due to high velocity targets under high bandwidth conditions), then a range bin migration term is introduced which couples to both the range term as well as the Doppler term. Due to this, the Doppler phase function is no more similar for all the ADC samples. It is a function of which ADC time sample is chosen. The keystone tranformation removes this coupling/dependency term (range bin migration term) through a time dependent(ADC time dimension) chirp axis scaling. This is accomplished through interpolation along the chirp dimension (for each ADC time sample) and then resampling on a new chirp grid where each ADC sample has a different chirp resampling factor. The interpolating factor for an FMCW RADAR architecture turns out to be (fc/(fc+St)) where fc is the chirp centre frequency, S is the slope of the chirp.
    
    For now, I have coded a single Doppler in a given range to test the Keystone algorithm that I had derived. It works perfectly fine! Currently, the keystone algorithm does not cater to velocities beyond +/-Fs/2. I need to understand why this is so. The reference paper has a separate section that deals with fixing the range bin migration when the velocity is greater than baseband velocity and has an "m" fold foldover to the baseband i.e. If v = v_b + mVs. In the subsequent commmits, I will generalize this script to cater to multiple Dopplers(all within +/-Fs/2) and with some noise. I will be interesting to see how well the interpolation works in the presence of noise.
    
    Signed-off-by: Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>

---
+++ radar_modeling/range_bin_migration/KeystoneAlgorithm/keystoneTransformation.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 17 00:16:44 2022
+
+@author: saiguna
+"""
+
+"""
+Reference paper used for Keystone transformation is available at the below link:
+    https://saigunaranjan.atlassian.net/wiki/spaces/RM/pages/4227073/Keystone+Transformation
+I have dervied and coded the keystone algorithm to cater to the FMCW architecture.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import interpolate
+from scipy.interpolate import interp1d
+
+
+
+plt.close('all')
+# np.random.seed(0)
+
+""" System Parameters """
+
+""" Below are the system parameters used for the 256v2J platform"""
+numSamples = 2048
+numFFTBins = 2048
+numDoppFFT = 2048
+thermalNoise = -174 # dBm/Hz
+noiseFigure = 10 # dB
+baseBandgain = 34 #dB
+adcSamplingRate = 56.25e6 # MHz
+dBFs_to_dBm = 10
+binSNR = 20 # dB
+lightSpeed = 3e8 # m/s
+
+""" Chirp Parameters"""
+chirpBW = 4e9 # Hz
+interRampTime = 44e-6#13e-6#44e-6 # us
+rampSamplingRate = 1/interRampTime
+
+chirpStartFreq = 77e9 # Giga Hz
+numChirps = 168#500#168
+
+
+""" Object parameters"""
+objectRange_m = 10
+objectVelocity_mps = 20#60 # 20 m/s
+
+
+adcSamplingTime = 1/adcSamplingRate # seconds
+chirpOnTime = numSamples*adcSamplingTime #39.2e-6
+chirpSamplingRate = 1/interRampTime
+chirpSlope = chirpBW/chirpOnTime
+chirpCentreFreq = chirpStartFreq + chirpBW/2
+lamda = lightSpeed/chirpCentreFreq
+maxVelBaseband_mps = (chirpSamplingRate/2) * (lamda/2) # m/s
+rangeRes = lightSpeed/(2*chirpBW)
+
+print('Max base band velocity = {0:.2f} m/s'.format(maxVelBaseband_mps))
+FsEquivalentVelocity = 2*maxVelBaseband_mps # Fs = 2*Fs/2
+velocityRes = (chirpSamplingRate/numChirps) * (lamda/2)
+print('Velocity resolution = {0:.2f} m/s'.format(velocityRes))
+
+
+rangeTerm = np.exp(1j*2*np.pi*(chirpSlope*2*objectRange_m/lightSpeed)*\
+                        adcSamplingTime*np.arange(numSamples)) # numSamples
+
+dopplerTerm = np.exp(1j*2*np.pi*(2*objectVelocity_mps/lamda)*interRampTime*np.arange(numChirps)) # DopplerBin x numChirps
+
+rangeBinMigrationTerm = np.exp(1j*2*np.pi*chirpSlope*2*(objectVelocity_mps/lightSpeed)*\
+                                    interRampTime*adcSamplingTime*\
+                                        np.arange(numChirps)[None,:]*np.arange(numSamples)[:,None]) # DopplerBin x numHyp x numChirps x numSamples
+
+radarSignal = rangeTerm[:,None] * dopplerTerm[None,:] * rangeBinMigrationTerm
+
+receivedSignal = radarSignal
+receivedSignal = receivedSignal*np.hanning(numSamples)[:,None]
+receivedSignalRfft = np.fft.fft(receivedSignal,axis=0)/numSamples
+receivedSignalRfft = receivedSignalRfft[0:numSamples//2,:]
+
+receivedSignalRfftDoppWin = receivedSignalRfft*np.hanning(numChirps)[None,:]
+receivedSignalRfftDoppFFT = np.fft.fft(receivedSignalRfftDoppWin,axis=1,n=numDoppFFT)/numChirps
+
+receivedSignalRfftMagSpec = 20*np.log10(np.abs(receivedSignalRfft))
+receivedSignalRfftDfftMagSpec = 20*np.log10(np.abs(receivedSignalRfftDoppFFT))
+
+# rangeAxis = np.arange(numSamples//2)*rangeRes
+chirpAxis = np.arange(numChirps)*interRampTime
+
+
+""" KeyStone Transformation"""
+# interpFact = ((chirpCentreFreq + chirpSlope*np.arange(numSamples)[:,None]*adcSamplingTime)/chirpCentreFreq)*interRampTime*np.arange(numChirps)[None,:]
+interpFact = (chirpCentreFreq/(chirpCentreFreq + chirpSlope*np.arange(numSamples)[:,None]*adcSamplingTime))*np.arange(numChirps)[None,:]*interRampTime
+
+allChirpsDuration = (numChirps-1)*interRampTime
+
+doppPhase = np.unwrap(np.angle(radarSignal),axis=1)
+chirpSampPoints = np.arange(numChirps)*interRampTime
+interpreceivedSignal = np.zeros((receivedSignal.shape),dtype=np.complex64)
+for ele in range(numSamples):
+    if 0:
+        doppPhaseInterpFunc = interp1d(chirpSampPoints, doppPhase[ele,:], kind='linear')
+        xnew = interpFact[ele,:]
+        doppPhaseInterpVals = doppPhaseInterpFunc(xnew)
+    if 1:
+        doppPhaseInterpFunc = interpolate.splrep(chirpSampPoints, doppPhase[ele,:], s=0, k=1)
+        xnew = interpFact[ele,:]
+        doppPhaseInterpVals = interpolate.splev(xnew, doppPhaseInterpFunc, der=0)
+
+    interpreceivedSignal[ele,:] = np.exp(1j*doppPhaseInterpVals)
+
+
+doppPhaseInterp = np.unwrap(np.angle(interpreceivedSignal),axis=1)
+interpreceivedSignalWind = interpreceivedSignal*np.hanning(numSamples)[:,None]
+interpreceivedSignalRfft = np.fft.fft(interpreceivedSignalWind,axis=0)/numSamples
+interpreceivedSignalRfft = interpreceivedSignalRfft[0:numSamples//2,:]
+
+interpreceivedSignalRfftDoppWin = interpreceivedSignalRfft*np.hanning(numChirps)[None,:]
+interpreceivedSignalRfftDoppFFT = np.fft.fft(interpreceivedSignalRfftDoppWin,axis=1,n=numDoppFFT)/numChirps
+
+interpreceivedSignalRfftMagSpec = 20*np.log10(np.abs(interpreceivedSignalRfft))
+interpreceivedSignalRfftDfftMagSpec = 20*np.log10(np.abs(interpreceivedSignalRfftDoppFFT))
+
+binsMovedPreCorr = np.argmax(receivedSignalRfftMagSpec,axis=0)
+binsMovedPostCorr = np.argmax(interpreceivedSignalRfftMagSpec,axis=0)
+
+
+plt.figure(1,figsize=(20,10),dpi=200)
+plt.subplot(2,2,1)
+plt.title('Range bin vs Chirp number')
+plt.imshow(receivedSignalRfftMagSpec[260:275,:])
+plt.xlabel('Chirp number')
+plt.ylabel('Range bin')
+plt.subplot(2,2,2)
+plt.title('Range-Doppler map')
+plt.imshow(receivedSignalRfftDfftMagSpec)
+plt.xlabel('Doppler bin')
+plt.ylabel('Range bin')
+plt.subplot(2,2,3)
+plt.imshow(interpreceivedSignalRfftMagSpec[260:275,:])
+plt.xlabel('Chirp number')
+plt.ylabel('Range bin')
+plt.subplot(2,2,4)
+plt.imshow(interpreceivedSignalRfftDfftMagSpec)
+plt.xlabel('Doppler bin')
+plt.ylabel('Range bin')
+
+plt.figure(3,figsize=(20,10),dpi=200)
+plt.title('Range bins moved across chirps for object with speed = ' + str(objectVelocity_mps) + ' mps.' + ' Chirp BW = ' + str(int(chirpBW/1e9)) + ' GHz')
+plt.plot(binsMovedPreCorr,'-o',label='Pre correction')
+plt.plot(binsMovedPostCorr,'-o',label='Post Keystone transformation')
+plt.xlabel('chirp number')
+plt.ylabel('range bins');
+plt.grid(True)
+plt.legend()
