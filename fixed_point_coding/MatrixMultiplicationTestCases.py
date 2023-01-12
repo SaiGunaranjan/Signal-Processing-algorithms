8a00a7713990e8abed0a4adf2850615575ad7f57
Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>
2023-01-13 00:45:46 +0530

    Perform fixed point complex matrix multiplications
    In this commit, I have brought up the fixed point complex matrix
    multiplication function. I have implemented it the same way as fixed
    point real matrix multiplication. Multiply two 32 bit numbers, drop 32 bits
    post this and keep accumulating. I have also tested this function using
    some complex matrix multiplication test cases as well.
    
    Signed-off-by: Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>

--- fixed_point_coding/MatrixMultiplicationTestCases.py
+++ fixed_point_coding/MatrixMultiplicationTestCases.py
@@ -8,9 +8,10 @@ Created on Fri Dec 30 20:33:51 2022
 
 import numpy as np
 from fixedPointLibrary import convert_float_to_fixedPointInt, dropFractionalBits_fixedPointInt,\
-    matrixMultiplicationFixedPoint
+    matrixMultiplicationFixedPoint, matrixMultiplicationFixedPointComplexInput
 
 # np.random.seed(10)
+print('\n\n Real Multiplication')
 
 numRowsA= 3
 numColsA = numRowsB = 8
@@ -30,6 +31,45 @@ B1fixed = convert_float_to_fixedPointInt(B1, numIntBits, numFracBits, numSignBit
 inputArrFracBits = numFracBits
 outputArrFracBits = inputArrFracBits
 C1fixed = matrixMultiplicationFixedPoint(A1fixed, B1fixed, inputArrFracBits, outputArrFracBits)
+C1fixedConvFloat = C1fixed/(2**outputArrFracBits)
+
+C1float = A1 @ B1
+
+
+print('Float result\n', C1float)
+print('Fixed result\n', C1fixedConvFloat)
+print('Total Error:\n',np.sum(C1float-C1fixedConvFloat))
+
+
+print('\n\n Complex Multiplication')
+
+numRowsA= 3
+numColsA = numRowsB = 8
+numColsB = 4
+A1 = np.random.uniform(-10,10,numRowsA*numColsA).reshape(numRowsA,numColsA) + 1j*np.random.uniform(-10,10,numRowsA*numColsA).reshape(numRowsA,numColsA)
+B1 = np.random.uniform(-12,12,numColsB*numRowsB).reshape(numRowsB,numColsB) + 1j*np.random.uniform(-12,12,numRowsB*numColsB).reshape(numRowsB,numColsB)
+
+realArray = np.hstack((A1.real.flatten(),B1.real.flatten()))
+imagArray = np.hstack((A1.imag.flatten(),B1.imag.flatten()))
+maxVal = np.amax(np.abs(np.hstack((realArray, imagArray))))
+
+totalBitWidth = 32
+numIntBits = np.ceil(np.log2(maxVal)).astype('int64') + 1 # (includes signed bit as well and hence the additonal 1) #4
+numFracBits = totalBitWidth - numIntBits #28
+numSignBits = 1
+
+A1fixedReal = convert_float_to_fixedPointInt(A1.real, numIntBits, numFracBits, numSignBits)
+A1fixedImag = convert_float_to_fixedPointInt(A1.imag, numIntBits, numFracBits, numSignBits)
+B1fixedReal = convert_float_to_fixedPointInt(B1.real, numIntBits, numFracBits, numSignBits)
+B1fixedImag = convert_float_to_fixedPointInt(B1.imag, numIntBits, numFracBits, numSignBits)
+
+A1fixed = A1fixedReal + 1j*A1fixedImag
+B1fixed = B1fixedReal + 1j*B1fixedImag
+
+
+inputArrFracBits = numFracBits
+outputArrFracBits = inputArrFracBits
+C1fixed = matrixMultiplicationFixedPointComplexInput(A1fixed, B1fixed, inputArrFracBits, outputArrFracBits)
 C1fixedConvFloat = C1fixed/(2**outputArrFracBits)
 
 C1float = A1 @ B1
--- fixed_point_coding/fixedPointLibrary.py
+++ fixed_point_coding/fixedPointLibrary.py
@@ -60,9 +60,11 @@ def addFractionalBits_fixedPointInt(inputFixedPointArr, inputArrFracBits, output
     return outputFixedPointArr
 
 
-def matrixMultiplicationFixedPoint_nonOptimal(A, B, inputArrFracBits, outputArrFracBits):
+def matrixMultiplicationFixedPoint_nonOptimalButPrecise(A, B, inputArrFracBits, outputArrFracBits):
 
     """
+
+
     This function does a fixed point matrix multiplication by dropping bits only at the final stage.
     This is not an optimal method of product and sum because, this would require a much larger bitwidth
     accumulator. For example, if the inputs are 32 bit numbers, then the product will be at max a 64 bit number.
@@ -70,8 +72,8 @@ def matrixMultiplicationFixedPoint_nonOptimal(A, B, inputArrFracBits, outputArrF
     which takes more area. Rather the more optimal way would be to drop 32 bits post multiplication itself.
     That way, the size of the accumulator would need to be only 32 bits.
 
-    Note: The accuracy with this method might be slighly higher though since we are working with
-    higher bitwidth numbers till the final stage.
+    Note: Do not use this function as this could lead to an overflow. If there is no overflow, the accuracy
+    with this method might be slighly higher since we are working with higher bitwidth numbers till the final stage.
 
     Input
      A: Fixed point matrix of dimension m x n and number of fractional bits = inputArrFracBits
@@ -88,7 +90,7 @@ def matrixMultiplicationFixedPoint_nonOptimal(A, B, inputArrFracBits, outputArrF
 
     numRowsC = A.shape[0]
     numColsC = B.shape[1]
-    C = np.zeros((numRowsC, numColsC),dtype = A.dtype)
+    C = np.zeros((numRowsC, numColsC),dtype = np.int64) # Assign maximum range even though it might still contain 32 bits
     numColsA = numRowsB = A.shape[1]
     bitGrowthDueToAddns = np.round(np.log2(numColsA)).astype('int32')
     inputFracBits = inputArrFracBits + inputArrFracBits # Assuming 1Q31 x 1Q31
@@ -135,7 +137,7 @@ def matrixMultiplicationFixedPoint(A, B, inputArrFracBits, outputArrFracBits):
     # C = np.zeros((numRowsC, numColsC),dtype = A.dtype)
     """ I have initialized the output matrix as int64 but it will contain int32 elements.
     If I initialize as int32, I was getting over flow errors"""
-    C = np.zeros((numRowsC, numColsC),dtype = np.int64) #
+    C = np.zeros((numRowsC, numColsC),dtype = np.int64) # Assign maximum range even though it might still contain 32 bits
     numColsA = numRowsB = A.shape[1]
     bitGrowthDueToAddns = np.ceil(np.log2(numColsA)).astype('int32')
     # bitGrowthDueToAddns = numColsA
@@ -156,6 +158,150 @@ def matrixMultiplicationFixedPoint(A, B, inputArrFracBits, outputArrFracBits):
                 sumVal += prodBitsDropped
             C[i,j] = sumVal
     return C
+
+
+
+def matrixMultiplicationFixedPointComplexInput(A, B, inputArrFracBits, outputArrFracBits):
+
+    """
+    This function does a fixed point matrix multiplication by dropping bits post each multiplication itself
+    and thus ensuring that the intermediate result is always limited to the outputArrFracBits.
+    This method of fixed point multiplication requires lesser area since the size of the accumulator is
+    limited to 32 bit unlike the previous method where the size of the accumulator is 64 bits.
+
+    The accuracy with this method might be slightly(very marginally) inferior to the previous function.
+
+    Input
+     A: Complex Fixed point matrix of dimension m x n and number of fractional bits = inputArrFracBits
+     B: Complex Fixed point matrix of dimension n x k and number of fractional bits = inputArrFracBits
+     inputArrFracBits: Number of fraction bits allocated for each of the input matrices A, B
+     outputArrFracBits: Number of fractional bits that must be allocated to the final result.
+    Output
+     C: Fixed point matrix of dimension m x k and number of fractional bits = outputArrFracBits
+
+
+     The fixed point numbers can be of form mQn like 1Q15 or 1Q31 or 1Q63
+
+    """
+
+    numRowsC = A.shape[0]
+    numColsC = B.shape[1]
+    # C = np.zeros((numRowsC, numColsC),dtype = A.dtype)
+    """ I have initialized the output matrix as int64 but it will contain int32 elements.
+    If I initialize as int32, I was getting over flow errors"""
+    C = np.zeros((numRowsC, numColsC),dtype = np.complex128) #
+    numColsA = numRowsB = A.shape[1]
+    bitGrowthDueToAddns = np.ceil(np.log2(numColsA)).astype('int32')
+    # bitGrowthDueToAddns = numColsA
+    inputFracBits = inputArrFracBits + inputArrFracBits # Assuming 1Q31 x 1Q31
+    """ Don't have accomodate for bit growth due to additions when the number of additions are small."""
+    outputFracBitsPostMult = outputArrFracBits #- bitGrowthDueToAddns
+    bitToBeDrop = inputFracBits - outputFracBitsPostMult
+
+    for i in range(numRowsC):
+        for j in range(numColsC):
+            realPart = 0
+            imagPart = 0
+            for k in range(numColsA):
+                """ The below product should be atleast int64 because 32 x 32 results in 64 bits and
+                hence to ensure the output of the product is 64 bits, atleast one of the input
+                has to be typecast as int64 so that the final output is also int64"""
+
+                realPart1 = ((A[i,k].real).astype('int64'))*((B[k,j].real).astype('int64'))
+                realPart1BitsDropped = dropFractionalBits_fixedPointInt(realPart1, inputFracBits, outputFracBitsPostMult)
+
+                realPart2 = ((A[i,k].imag).astype('int64'))*((B[k,j].imag).astype('int64'))
+                realPart2BitsDropped = dropFractionalBits_fixedPointInt(realPart2, inputFracBits, outputFracBitsPostMult)
+
+                realPart += (realPart1BitsDropped - realPart2BitsDropped)
+
+                imagPart1 = ((A[i,k].imag).astype('int64'))*((B[k,j].real).astype('int64'))
+                imagPart1BitsDropped = dropFractionalBits_fixedPointInt(imagPart1, inputFracBits, outputFracBitsPostMult)
+
+                imagPart2 = ((A[i,k].real).astype('int64'))*((B[k,j].imag).astype('int64'))
+                imagPart2BitsDropped = dropFractionalBits_fixedPointInt(imagPart2, inputFracBits, outputFracBitsPostMult)
+
+                imagPart += (imagPart1BitsDropped + imagPart2BitsDropped)
+
+            C[i,j] = realPart + 1j*imagPart
+    return C
+
+
+def matrixMultiplicationFixedPointComplexInput_nonOptimalButPrecise(A, B, inputArrFracBits, outputArrFracBits):
+
+    """
+    This function does a fixed point complex matrix multiplication by dropping bits only at the final stage.
+    This is not an optimal method of product and sum because, this would require a much larger bitwidth
+    accumulator. For example, if the inputs are 32 bit numbers, then the product will be at max a 64 bit number.
+    For a complex multipication, there is another addition as well. So ther will be a further bit growth due to this.
+    Now, if we were to sum/accumulate these products, then the accumulator should be of bit width 64
+    which takes more area. Rather the more optimal way would be to drop 32 bits post multiplication itself.
+    That way, the size of the accumulator would need to be only 32 bits.
+
+    Note: Do not use this function as this could lead to an overflow. There are 2 product terms and each product
+    results in a 64 bit number. On top of this, there is another addition term. Since each addition can result
+    in 1 bit growth, the resultant number will become greater than 64 bits and there will be an overflow.
+    If there is no overflow, the accuracy with this method might be slighly higher since we are working with higher
+    bitwidth numbers till the final stage.
+
+    Input
+     A: Complex Fixed point matrix of dimension m x n and number of fractional bits = inputArrFracBits
+     B: Complex Fixed point matrix of dimension n x k and number of fractional bits = inputArrFracBits
+     inputArrFracBits: Number of fraction bits allocated for each of the input matrices A, B
+     outputArrFracBits: Number of fractional bits that must be allocated to the final result.
+    Output
+     C: Fixed point matrix of dimension m x k and number of fractional bits = outputArrFracBits
+
+
+     The fixed point numbers can be of form mQn like 1Q15 or 1Q31 or 1Q63
+
+    """
+
+    numRowsC = A.shape[0]
+    numColsC = B.shape[1]
+    # C = np.zeros((numRowsC, numColsC),dtype = A.dtype)
+    """ I have initialized the output matrix as int64 but it will contain int32 elements.
+    If I initialize as int32, I was getting over flow errors"""
+    C = np.zeros((numRowsC, numColsC),dtype = np.complex128) #
+    numColsA = numRowsB = A.shape[1]
+    bitGrowthDueToAddns = np.ceil(np.log2(numColsA)).astype('int32')
+    # bitGrowthDueToAddns = numColsA
+    inputFracBits = inputArrFracBits + inputArrFracBits # Assuming 1Q31 x 1Q31
+    """ Don't have accomodate for bit growth due to additions when the number of additions are small."""
+    outputFracBitsPostMult = outputArrFracBits #- bitGrowthDueToAddns
+    bitToBeDrop = inputFracBits - outputFracBitsPostMult
+
+    for i in range(numRowsC):
+        for j in range(numColsC):
+            realPart = np.int(0)
+            imagPart = np.int(0)
+            for k in range(numColsA):
+                """ The below product should be atleast int64 because 32 x 32 results in 64 bits and
+                hence to ensure the output of the product is 64 bits, atleast one of the input
+                has to be typecast as int64 so that the final output is also int64"""
+
+                realPart1 = ((A[i,k].real).astype('int64'))*((B[k,j].real).astype('int64'))
+                realPart2 = ((A[i,k].imag).astype('int64'))*((B[k,j].imag).astype('int64'))
+                realPart += (realPart1 - realPart2) # Bit growth due to addition expected at each iteration.
+
+                # if (np.abs(realPart) > 2**32) and (np.abs(realPart) < 2**64):
+                #     print('Greater than a 32 bit number', k)
+                # print(np.abs(realPart) - 2**32)
+                # print('bitWidth',np.ceil(np.log2(np.abs(realPart))))
+                # if (np.abs(realPart) > 2**62):
+                #     print('Greater than a 63 bit number', k)
+
+                imagPart1 = ((A[i,k].imag).astype('int64'))*((B[k,j].real).astype('int64'))
+                imagPart2 = ((A[i,k].real).astype('int64'))*((B[k,j].imag).astype('int64'))
+                imagPart += (imagPart1 + imagPart2) # Bit growth due to addition expected at each iteration.
+
+            realPartBitsDropped = dropFractionalBits_fixedPointInt(realPart, inputFracBits, outputFracBitsPostMult)
+            imagPartBitsDropped = dropFractionalBits_fixedPointInt(imagPart, inputFracBits, outputFracBitsPostMult)
+            C[i,j] = realPartBitsDropped + 1j*imagPartBitsDropped
+    return C
+
+
+
 
 
 
