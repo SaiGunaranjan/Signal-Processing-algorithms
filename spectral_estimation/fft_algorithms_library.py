756fa93bf0640d1594b7653691a301956bbd9c37
Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>
2023-01-26 14:24:45 +0530

    Added a comparision with matrix multiplication based DFT of the signal
    Signed-off-by: Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>

--- spectral_estimation/fft_algorithms_library.py
+++ spectral_estimation/fft_algorithms_library.py
@@ -32,19 +32,34 @@ def fft_as_successive_dft(signal):
         return signalFFT
 
 
+def DFT(signal):
+
+    numSamples = len(signal)
+    dftMatrix = np.exp(-1j*2*np.pi*np.arange(numSamples)[:,None]*np.arange(numSamples)[None,:]/numSamples)
+    signaldft = dftMatrix @ signal
+
+    return signaldft
 
 numSamples = 512
 signalFreqBin = np.random.randint(0,numSamples)
 signal = np.exp(1j*2*np.pi*signalFreqBin*np.arange(numSamples)/numSamples)
 
-
 signalfft1 = np.fft.fft(signal)
 signalfft2 = fft_as_successive_dft(signal)
+signaldft = DFT(signal)
+
+""" Timing the modules
+Execute the following command on the terminal
+%timeit DFT(signal)
+%timeit fft_as_successive_dft(signal)
+%timeit np.fft.fft(signal)
+
+"""
 
 
 plt.figure(1,figsize=(20,10),dpi=200)
 plt.plot(20*np.log10(np.abs(signalfft1)),label='numpy fft')
-plt.plot(20*np.log10(np.abs(signalfft2)),lw=2,alpha=0.5,label='fft as successive dft')
-
+plt.plot(20*np.log10(np.abs(signaldft)),label='DFT')
+plt.plot(20*np.log10(np.abs(signalfft2)),lw=2,alpha=0.5,label='FFT as successive DFT')
 plt.grid(True)
 plt.legend()
