848859f33ef3d567009075ed9ce3b45063cf1766
Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>
2023-01-26 12:43:32 +0530

    FFT implemented as a successive DFT
    In this commit, I have implemented the first version of the Fast Fourier
    Transform algorithm. In this, the FFT is implemented as successive DFT.
    If x[n] is the time domain signal of length N(with N being a power of 2) and X[k] is its DFT, then X[k] can be
    written as G[k] + W(N,k)H[k], where G[k] is the N/2 point DFT of the even index
    terms of x[n] and H[k] is the N/2 point DFT of the odd index terms of x[n], W(N,K) is the twiddle factor. So, X[k] =  G[k] + W(N,k)H[k], for 0<=k<=N/2 -1. Now, since G[k], H[k] are the N/2 point DFT of x_even[n], x_odd[n] respectively, G[N/2 +k] = G[k] (and similarly for H[k]) due to the periodic property of DFT. i.e. If X[k] is the N point DFT of x[n], then X[N+k] = X[k]. So it turn out, for N/2 <=k <= N-1, X[k] = G[k] - W(N,k)H[k]. So, to summarize,
    X[k] =  G[k] + W(N,k)H[k] for 0 <=k <= N/2 -1,
    X[k] =  G[k] - W(N,k)H[k] for N/2 <= k< = N -1
    So, if we compute G[k], W(N,k)H[k] for 0 <=k <= N/2 -1, we have the data
    for N/2 <= k< = N -1 as well. Thus we are saving some compute.
    Now, G[k] is the N/2 point DFT of even indexed terms. This even index
    terms can again be split into even of the even idex terms and odd of the
    even index terms. Then we can perform separate N/4 point DFTs for the x_even_even[n] and x_odd_even[n]. So G[k],H[k] can be further split into 2 N/4 point DFTs. Thus we can succesively keep breaking up the DFTs into smaller and smaller DFT like a recursion. This concept has been beautifully explained by Oppenheim in his MIT video lectures. I have derived this structure myself as well.
    
    Signed-off-by: Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>

---
+++ spectral_estimation/fft_algorithms_library.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 25 23:43:07 2023
+
+@author: Sai Gunaranjan
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def fft_as_successive_dft(signal):
+
+    if (len(signal) == 1):
+        return signal
+    else:
+        evenIndexSignal = signal[0::2]
+        oddIndexSignal = signal[1::2]
+        evenIndexSignalFFT = fft_as_successive_dft(evenIndexSignal)
+        oddIndexSignalFFT = fft_as_successive_dft(oddIndexSignal)
+
+        N = 2*len(evenIndexSignalFFT)
+        k = np.arange(0,N/2)
+
+        twiddleFactor = np.exp(-1j*2*np.pi*k/N)
+
+        oddIndexSignalFFTTwiddleScaling = oddIndexSignalFFT*twiddleFactor
+        fftTopHalf = evenIndexSignalFFT + oddIndexSignalFFTTwiddleScaling
+        fftBottomHalf = evenIndexSignalFFT - oddIndexSignalFFTTwiddleScaling
+
+        signalFFT = np.hstack((fftTopHalf,fftBottomHalf))
+
+        return signalFFT
+
+
+
+numSamples = 512
+signalFreqBin = np.random.randint(0,numSamples)
+signal = np.exp(1j*2*np.pi*signalFreqBin*np.arange(numSamples)/numSamples)
+
+
+signalfft1 = np.fft.fft(signal)
+signalfft2 = fft_as_successive_dft(signal)
+
+
+plt.figure(1,figsize=(20,10),dpi=200)
+plt.plot(20*np.log10(np.abs(signalfft1)),label='numpy fft')
+plt.plot(20*np.log10(np.abs(signalfft2)),lw=2,alpha=0.5,label='fft as successive dft')
+
+plt.grid(True)
+plt.legend()
