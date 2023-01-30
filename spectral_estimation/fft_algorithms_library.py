ad01b1df3c5443ab260c744854a34e977ca8b334
Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>
2023-01-30 23:06:34 +0530

    Compute scaled FFT using successive DFT method
    In this commit, I have written a modified version of the FFT as a
    successive DFT to ensure the output is scaled by N. The way to do this
    is as follows. There are log2(N) stages of successive DFTs. Now, at each
    stage, if we scale the ouput by a factor of 2, then the overall scaling
    for the log2(N) stages becomes 2*2*....(log2(N) times) = 2**(log2(N)) =
    N. Hence, if we scale the output at each stage by 2, then we can achieve
    a normalized FFT.
    
    Signed-off-by: Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>

--- spectral_estimation/fft_algorithms_library.py
+++ spectral_estimation/fft_algorithms_library.py
@@ -5,12 +5,15 @@ Created on Wed Jan 25 23:43:07 2023
 @author: Sai Gunaranjan
 """
 
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 def fft_as_successive_dft(signal):
 
-    if (len(signal) == 1):
+    N = len(signal)
+
+    if (N == 1):
         return signal
     else:
         evenIndexSignal = signal[0::2]
@@ -18,7 +21,7 @@ def fft_as_successive_dft(signal):
         evenIndexSignalFFT = fft_as_successive_dft(evenIndexSignal)
         oddIndexSignalFFT = fft_as_successive_dft(oddIndexSignal)
 
-        N = 2*len(evenIndexSignalFFT)
+        # N = 2*len(evenIndexSignalFFT)
         k = np.arange(0,N/2)
 
         twiddleFactor = np.exp(-1j*2*np.pi*k/N)
@@ -31,6 +34,32 @@ def fft_as_successive_dft(signal):
 
         return signalFFT
 
+def fft_as_successive_dft_scaled(signal):
+
+    if (len(signal) == 1):
+        return signal
+    else:
+        evenIndexSignal = signal[0::2]
+        oddIndexSignal = signal[1::2]
+        evenIndexSignalFFT = fft_as_successive_dft_scaled(evenIndexSignal)
+        oddIndexSignalFFT = fft_as_successive_dft_scaled(oddIndexSignal)
+
+        N = 2*len(evenIndexSignalFFT)
+        k = np.arange(0,N/2)
+
+        twiddleFactor = np.exp(-1j*2*np.pi*k/N)
+
+        oddIndexSignalFFTTwiddleScaling = oddIndexSignalFFT*twiddleFactor
+        fftTopHalf = evenIndexSignalFFT + oddIndexSignalFFTTwiddleScaling
+        fftBottomHalf = evenIndexSignalFFT - oddIndexSignalFFTTwiddleScaling
+
+        signalFFT = np.hstack((fftTopHalf,fftBottomHalf))/2 # By 2 to scale the ouput at each stage.
+        # Since there are log2(N) stages, the scaling becomes 2**(log2(N)) = N
+
+        return signalFFT
+
+
+
 
 def DFT(signal):
 
@@ -40,16 +69,19 @@ def DFT(signal):
 
     return signaldft
 
+plt.close('all')
 numSamples = 512
 signalFreqBin = np.random.randint(0,numSamples)
 signal = np.exp(1j*2*np.pi*signalFreqBin*np.arange(numSamples)/numSamples)
 
 signalfft1 = np.fft.fft(signal)
 signalfft2 = fft_as_successive_dft(signal)
+signalfft3 = fft_as_successive_dft_scaled(signal)
 signaldft = DFT(signal)
 
 """ Timing the modules
-Execute the following command on the terminal
+Execute the following command on the terminal to get the timings for different implementations of the fourier transform
+
 %timeit DFT(signal)
 %timeit fft_as_successive_dft(signal)
 %timeit np.fft.fft(signal)
@@ -59,7 +91,8 @@ Execute the following command on the terminal
 
 plt.figure(1,figsize=(20,10),dpi=200)
 plt.plot(20*np.log10(np.abs(signalfft1)),label='numpy fft')
-plt.plot(20*np.log10(np.abs(signaldft)),label='DFT')
+# plt.plot(20*np.log10(np.abs(signaldft)),label='DFT')
 plt.plot(20*np.log10(np.abs(signalfft2)),lw=2,alpha=0.5,label='FFT as successive DFT')
+plt.plot(20*np.log10(np.abs(signalfft3)),alpha=0.5,label='FFT as successive DFT scaled')
 plt.grid(True)
 plt.legend()
