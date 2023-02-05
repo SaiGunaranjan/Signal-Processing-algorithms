44c7962383a4ebac03ff48133725a894685961f6
Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>
2023-02-05 12:22:03 +0530

    Fixed point radix-2 fft implementation
    1. Changed the number of fractional bits from 31 to 15 to model a 16 bit ADC instead of a 32 bit ADC.
    2. Also, the window function is quantized to a 16 bit fixed point number.
    3. Disabled the DFT computation since it is time consuming and not the optimal method for fixed point FFT comparisons.
    4. Implemented the fixed point radix-2 fft.
    5. Also implemented the fixed point normalized radix-2 fft which scales the output by N.
    6. Compared the dynamic range/noise floor set by the radix-2 normalized FFT against the theoretical value. The theoretical value for the fixed point normalized radix 2 fft is given as
    N/S ratio  = 4N2^(-2b), where N/S is the noise to signal ratio, N is the number of samples of the signal, b is the number of bits for the fixed pointing of the normalized radix-2 fft. It is also the number of bits of the input signal (on which FFT needs to be done). The noise here is the total noise integrated over all the bins (or over the entire Fs). The formula can be re-written as Signal/(Total Noise) ratio = 2^(2b)/(4N). If we want the bin SNR, then we normalize the formula with N and hence the formula gets modified as bin SNR = 2^(2b)/4. Thus the bin SNR is independent of N while the total SNR is dependent on the number of samples of the signal. It makes sense because, as the number of samples increases, the number of radix-2 stages increases. This has two repurcussions. One on the signal gain and on the quantization noise floor. As N increases, the signal gain also increases. As N increases, the number of FFT stages increases leading to more FFT quantization noise added at each stage. Hence, the dynamic range/noise floor/signal to total noise ratio is dependent on the number of sampeles N.
    Using this formula we can estimate the dynamic range/noise floor for a b bit fixed point FFT. This formula can be found in
    Page 668 of the book Discrete-time signal processing by Oppenheim and Schafer(https://research.iaun.ac.ir/pd/naghsh/pdfs/UploadFile_2230.pdf), Eq 9.69
    
    Now, if the ADC signal (or fixed point signal) qunatization noise floor is greater than the floor set by the fixed point fft, then the overall floor is limited by the ADC qunatization noise. But if the ADC qunatization noise is very low, then the noise floor will be limted by the fixed point FFT qunatization. I will add more informative plots in the next few commits.
    
    Signed-off-by: Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>

--- fixed_point_coding/snrToneGeneration_fixedPoint.py
+++ fixed_point_coding/snrToneGeneration_fixedPoint.py
@@ -6,10 +6,16 @@ Created on Thu Dec 29 21:13:23 2022
 """
 
 
+"""
+Theoretical dynamic range set by a fixed point radix 2 normalized fft is given in the reference below:
+Page 668 of the book Discrete-time signal processing by Oppenheim and Schafer(https://research.iaun.ac.ir/pd/naghsh/pdfs/UploadFile_2230.pdf), Eq 9.69
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 from fixedPointLibrary import convert_float_to_fixedPointInt, dropFractionalBits_fixedPointInt,\
-    convert_Complexfloat_to_fixedPointInt, matrixMultiplicationFixedPointComplexInput, fixedPointDFT
+    convert_Complexfloat_to_fixedPointInt, fixedPointDFT, fixedPointfft_as_successive_dft, \
+        fixedPointfft_as_successive_dft_scaled
 import time as time
 
 
@@ -33,8 +39,6 @@ totalNoiseSigma = np.sqrt(totalNoisePower)
 """
 windowFunction = np.hanning(numTimeSamples)
 # dynamicRangedB = 20 # Eg. -10 dBsm and +10 dBsm RCS targets have an effective dynamic range of 20 dB
-# referenceSNR = 20
-# objectSNR_perBin = np.array([referenceSNR+dynamicRangedB])
 objectSNR_perBin = np.array([40])
 signalPowerdBFs = objectSNR_perBin + noiseFloorPerBindBFs
 signalPower = 10**(signalPowerdBFs/10)
@@ -54,13 +58,13 @@ rfft = np.fft.fft(signalWindowedFloat)[0:numRangeSamples]/numTimeSamples
 """ Fixed point implementation"""
 
 IntBitsSignal = 1
-FracBitsSignal = 31
+FracBitsSignal = 15 # 31
 signBitSignal = 1
 
 noisySignalFixedPoint = convert_Complexfloat_to_fixedPointInt(noisySignal,IntBitsSignal,FracBitsSignal,signBitSignal)
 
 IntBitsWindow = 1
-FracBitsWindow = 31
+FracBitsWindow = 15 #31
 signBitWindow = 1
 windowFunctionFixedPoint = convert_float_to_fixedPointInt(windowFunction,IntBitsWindow,FracBitsWindow,signBitWindow) # 1Q31
 
@@ -75,8 +79,8 @@ We need to bring down 2**62 to 2**31. Hence we need to drop 62-31 = 31 bits.
 Since the output has a swing from +/-2**31, to get back the true float value, divide by 2**31
 
 """
-inputArrFracBits = 62
-outputArrFracBits = 31
+inputArrFracBits = FracBitsSignal + FracBitsWindow#30 # 62
+outputArrFracBits = FracBitsSignal#15 # 31
 signalWindowedRealBitsDropped = dropFractionalBits_fixedPointInt(np.real(signalWindowedFixedPoint).astype('int64'), inputArrFracBits, outputArrFracBits)
 signalWindowedImagBitsDropped = dropFractionalBits_fixedPointInt(np.imag(signalWindowedFixedPoint).astype('int64'), inputArrFracBits, outputArrFracBits)
 """ The signal passed to the above function should be of datatype int64. This is beacause,
@@ -86,78 +90,97 @@ to the max 32 bit signed value i.e. -2**31, 2**31-1
 signalWindowedBitsDropped = signalWindowedRealBitsDropped + 1j*signalWindowedImagBitsDropped
 signalWindowedCovertFloat = signalWindowedBitsDropped/(2**outputArrFracBits)
 
-""" Fourier transform using matrix based DFT """
-timeInd = np.arange(numTimeSamples)
-numFFTBins = numTimeSamples # For now assume same number of FFT bins as number of samples.
-freqInd = np.arange(numFFTBins)
-#Also, assume for now that the signal length is always a power of 2. This is because, for fixed point integer operations, it is easy to work with scale factors which are powers of 2
-DFTMatrix = np.exp(-1j*2*np.pi*freqInd[:,None]*timeInd[None,:]/numFFTBins)
 
-rfft_dftMethod = (DFTMatrix @ signalWindowedFloat)/numTimeSamples
-rfft_dftMethod = rfft_dftMethod[0:numRangeSamples]
 
-inputArrFracBits = 31
-outputArrFracBits = 31#31
-t1 = time.time()
-rfft_dftFixedPoint = fixedPointDFT(signalWindowedBitsDropped, numFFTBins, inputArrFracBits, outputArrFracBits)
-t2 = time.time()
-rfft_dftFixedPoint = (rfft_dftFixedPoint[0:numRangeSamples].squeeze())/numTimeSamples
-rfft_dftFixedPointConvertToFloat = rfft_dftFixedPoint/(2**outputArrFracBits)
+""" Disable DFT since compute time is high and is practically not implementable"""
+if 0:
 
-timeForFixedPointDFT = t2-t1
-print('Total time for Fixed point DFT = {0:.2f} sec'.format(timeForFixedPointDFT))
+    """ Fourier transform using matrix based DFT """
+    timeInd = np.arange(numTimeSamples)
+    numFFTBins = numTimeSamples # For now assume same number of FFT bins as number of samples.
+    freqInd = np.arange(numFFTBins)
+    #Also, assume for now that the signal length is always a power of 2. This is because, for fixed point integer operations, it is easy to work with scale factors which are powers of 2
+    DFTMatrix = np.exp(-1j*2*np.pi*freqInd[:,None]*timeInd[None,:]/numFFTBins)
+    rfft_dftMethod = (DFTMatrix @ signalWindowedFloat)/numTimeSamples
+    rfft_dftMethod = rfft_dftMethod[0:numRangeSamples]
+    rfft_dftMethodSpec = 20*np.log10(np.abs(rfft_dftMethod))
+    # rfft_dftMethodSpec -= np.amax(rfft_dftMethodSpec)
+
+    """ Fixed point DFT """
+    inputArrFracBits = FracBitsSignal#15 #31
+    outputArrFracBits = FracBitsSignal#15 #31
+    t1 = time.time()
+    rfft_dftFixedPoint = fixedPointDFT(signalWindowedBitsDropped, numFFTBins, inputArrFracBits, outputArrFracBits)
+    t2 = time.time()
+    rfft_dftFixedPoint = (rfft_dftFixedPoint[0:numRangeSamples].squeeze())/numTimeSamples
+    rfft_dftFixedPointConvertToFloat = rfft_dftFixedPoint/(2**outputArrFracBits)
+    rfft_dftFixedPointConvertToFloatSpec = 20*np.log10(np.abs(rfft_dftFixedPointConvertToFloat))
+    # rfft_dftFixedPointConvertToFloatSpec -= np.amax(rfft_dftFixedPointConvertToFloatSpec)
+    timeForFixedPointDFT = t2-t1
+    print('Total time for Fixed point DFT = {0:.2f} sec'.format(timeForFixedPointDFT))
+
+""" Fixed Point Radix-2 FFT"""
+radix2fftFixedPoint = fixedPointfft_as_successive_dft(signalWindowedBitsDropped, IntBitsSignal, FracBitsSignal, signBitSignal)
+radix2fftFixedPoint = (radix2fftFixedPoint[0:numRangeSamples].squeeze())/numTimeSamples
+radix2fftFixedPointConvertToFloat = radix2fftFixedPoint/(2**FracBitsSignal)
+
+""" Fixed Point Normalized Radix-2 FFT"""
+radix2fftNormalizedFixedPoint = fixedPointfft_as_successive_dft_scaled(signalWindowedBitsDropped, IntBitsSignal, FracBitsSignal, signBitSignal)
+radix2fftNormalizedFixedPoint = (radix2fftNormalizedFixedPoint[0:numRangeSamples].squeeze())
+""" Take care of 0 values in the FFT """
+radix2fftNormalizedFixedPoint[(np.real(radix2fftNormalizedFixedPoint) == 0) & (np.imag(radix2fftNormalizedFixedPoint)==0)] = 1
+""" """
+radix2fftNormalizedFixedPointConvertToFloat = radix2fftNormalizedFixedPoint/(2**FracBitsSignal)
+radix2fftNormalizedFixedPointConvertToFloatSpec = 20*np.log10(np.abs(radix2fftNormalizedFixedPointConvertToFloat))
 
 rfftSpec = 20*np.log10(np.abs(rfft))
-rfftSpec -= np.amax(rfftSpec)
+# rfftSpec -= np.amax(rfftSpec)
 
-rfft_dftMethodSpec = 20*np.log10(np.abs(rfft_dftMethod))
-rfft_dftMethodSpec -= np.amax(rfft_dftMethodSpec)
+radix2fftFixedPointConvertToFloatSpec = 20*np.log10(np.abs(radix2fftFixedPointConvertToFloat))
+# radix2fftFixedPointConvertToFloatSpec -= np.amax(radix2fftFixedPointConvertToFloatSpec)
 
-rfft_dftFixedPointConvertToFloatSpec = 20*np.log10(np.abs(rfft_dftFixedPointConvertToFloat))
-rfft_dftFixedPointConvertToFloatSpec -= np.amax(rfft_dftFixedPointConvertToFloatSpec)
+numBits = IntBitsSignal + FracBitsSignal
+theoreticalDynamicRange = 4*2**(-2*numBits)
+theoreticalDynamicRangedB = 10*np.log10(theoreticalDynamicRange)
+theoreticalDynamicRangedB = np.round(theoreticalDynamicRangedB)
 
-
-plt.figure(1,figsize=(20,10))
-plt.suptitle('Windowed Signal: Floating vs Fixed')
-plt.subplot(2,2,1)
-plt.title('Real part')
-plt.plot(np.real(signalWindowedFloat[numTimeSamples//2 - 50:numTimeSamples//2 + 50]),label='Floating Point')
-plt.plot(np.real(signalWindowedCovertFloat[numTimeSamples//2 - 50:numTimeSamples//2 + 50]),label='Fixed Point convert to Float')
-plt.grid(True)
-plt.legend()
-
-plt.subplot(2,2,2)
-plt.title('Imag part')
-plt.plot(np.imag(signalWindowedFloat[numTimeSamples//2 - 50:numTimeSamples//2 + 50]),label='Floating Point')
-plt.plot(np.imag(signalWindowedCovertFloat[numTimeSamples//2 - 50:numTimeSamples//2 + 50]),label='Fixed Point convert to Float')
-plt.grid(True)
-plt.legend()
-
-
-plt.subplot(2,2,3)
-plt.title('Real part error')
-plt.plot(np.real(signalWindowedFloat) - np.real(signalWindowedCovertFloat))
-plt.grid(True)
-
-
-plt.subplot(2,2,4)
-plt.title('Imag part error')
-plt.plot(np.imag(signalWindowedFloat) - np.imag(signalWindowedCovertFloat))
-plt.grid(True)
 
 plt.figure(2,figsize=(20,10))
 plt.title('Range spectrum')
-plt.plot(rfftSpec,label='Spectrum using FFT')
-plt.plot(rfft_dftMethodSpec,alpha=0.5,label='Spectrum using DFT')
-plt.plot(rfft_dftFixedPointConvertToFloatSpec,label='Spectrum using ' + str(outputArrFracBits) + ' bit Fixed Point DFT')
+plt.plot(rfftSpec,lw=6,alpha=0.3,label='Spectrum using FFT')
+# plt.plot(rfft_dftMethodSpec,lw=4,alpha=0.5,label='Spectrum using DFT')
+# plt.plot(rfft_dftFixedPointConvertToFloatSpec,label='Spectrum using ' + str(outputArrFracBits) + ' bit Fixed Point DFT')
+plt.plot(radix2fftFixedPointConvertToFloatSpec,label='Spectrum using ' + str(outputArrFracBits) + ' bit Fixed Point Radix-2 FFT')
+plt.plot(radix2fftNormalizedFixedPointConvertToFloatSpec,label='Spectrum using ' + str(outputArrFracBits) + ' bit Fixed Point Normalized Radix-2 FFT')
+plt.axhline(theoreticalDynamicRangedB,color='k',label='Theoret. DR with ' + str(numBits) + ' bits = ' + str(theoreticalDynamicRangedB) + 'dB')
 plt.xlabel('bins')
 plt.grid('True')
 plt.legend()
 
 
+if 0:
+    plt.figure(1,figsize=(20,10))
+    plt.suptitle('Windowed Signal: Floating vs Fixed')
+    plt.subplot(2,2,1)
+    plt.title('Real part')
+    plt.plot(np.real(signalWindowedFloat[numTimeSamples//2 - 50:numTimeSamples//2 + 50]),label='Floating Point')
+    plt.plot(np.real(signalWindowedCovertFloat[numTimeSamples//2 - 50:numTimeSamples//2 + 50]),label='Fixed Point convert to Float')
+    plt.grid(True)
+    plt.legend()
 
+    plt.subplot(2,2,2)
+    plt.title('Imag part')
+    plt.plot(np.imag(signalWindowedFloat[numTimeSamples//2 - 50:numTimeSamples//2 + 50]),label='Floating Point')
+    plt.plot(np.imag(signalWindowedCovertFloat[numTimeSamples//2 - 50:numTimeSamples//2 + 50]),label='Fixed Point convert to Float')
+    plt.grid(True)
+    plt.legend()
 
-# plt.figure(1,figsize=(20,10))
-# plt.title('Range Spectrum')
-# plt.plot(20*np.log10(np.abs(rfft)))
-# plt.grid(True)
+    plt.subplot(2,2,3)
+    plt.title('Real part error')
+    plt.plot(np.real(signalWindowedFloat) - np.real(signalWindowedCovertFloat))
+    plt.grid(True)
+
+    plt.subplot(2,2,4)
+    plt.title('Imag part error')
+    plt.plot(np.imag(signalWindowedFloat) - np.imag(signalWindowedCovertFloat))
+    plt.grid(True)
