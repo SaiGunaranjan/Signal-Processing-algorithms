54e9ee93191c06f436ab9c8ce97e97484e191506
Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>
2024-04-14 23:22:16 +0530

    Introduce quantization in DFFT output
    In this commit, I have introduced the DFFT with and without quantization. To model/simulate the DFFT with quantization, I take the quantized(with the respective bitwidths) RFFT output and perform clean floating point Doppler FFT and then quantize this DFFT output to the necessary bitwidths. I assume, RFFT, DFFT (and eventually Rx FFT) all work with the same bitwidths and hence the same chain is maintained. The SNR gain post DFFT is 10*np.log10(numChirps) and hence if we normalize the DFFT output (i.e. divide DFFT output by numChirps), then the signal power post DFFT is maintained the same as it was post RFFT. So, the noise floor post DFFT scales by the SNR gain i.e 10*np.log10(numChirps). If the noise floor post RFFT ws -90 dB, then post DFFT(and normalization), the noise floor becomes -90 - 10*np.log10(numChirps). This is the theoretical noisefloor. However, the quantized DFFT output is limited by the 1LSB i.e 2**(-B) again (since DFFT also works on the same bitwidth as RFFT). So, here also, the samllest power value that can be represented is 2**(2*-B) and hence the Doppler floor on the log scale is again 10*np.log10(2**(2*-B)). So, all power values less than 2**(-2B) will underflow to 0. Now, if we have a signal/target whose power is below 10*np.log10(2**(2*-B)), then that target will be lost due to underflows and will not show up even post RX FFT.
    
    Signed-off-by: Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>

--- fixed_point_coding/bitwidth_dynamicrange_analysis.py
+++ fixed_point_coding/bitwidth_dynamicrange_analysis.py
@@ -17,6 +17,9 @@ numBitsRangeFFTOutput = np.array([12,16,18,20,24,32],dtype=np.int64)
 numFracBitsRangeFFTOutput = numBitsRangeFFTOutput-1 # -1 for the integer/sign bit
 numTestCases = len(numBitsRangeFFTOutput)
 
+theoretRangeFloorValQuant = -10*np.log10(2**(2*numFracBitsRangeFFTOutput))
+
+""" System parameters"""
 numADCSamples = 2048
 numRangeSamples = numADCSamples//2
 numChirps = 512
@@ -70,7 +73,6 @@ for ele in range(numTestCases):
 
 
 flagRfftPowMean = 1
-
 if flagRfftPowMean:
     rfftSpecdB = 10*np.log10(np.mean(np.abs(rfft)**2,axis=(1,2)))
 
@@ -83,8 +85,35 @@ else:
     rfftfpconvfloatSpecdB = rfftfpconvfloatSpecdB[:,:,0,0]
 
 
+dfft = np.fft.fft(rfft,axis=1)/numChirps
 
-theoretRangeFloorValQuant = -10*np.log10(2**(2*numFracBitsRangeFFTOutput))
+""" Perform DFFT on the quantized RFFT output"""
+dfftfp = np.fft.fft(rfftfpconvfloatAllBitWidths,axis=2)/numChirps
+
+dfftfpconvfloatAllBitWidths = np.zeros((numTestCases,rfft.shape[0],rfft.shape[1],rfft.shape[2]),dtype=np.complex128)
+for ele in range(numTestCases):
+
+    dfftfp_ = np.floor(dfftfp[ele,:,:,:].real * 2**numFracBitsRangeFFTOutput[ele] + 0*0.5) + \
+        1j*np.floor(dfftfp[ele,:,:,:].imag * 2**numFracBitsRangeFFTOutput[ele] + 0*0.5)
+
+    dfftfpconvfloat = dfftfp_/(2**numFracBitsRangeFFTOutput[ele])
+    dfftfpconvfloatAllBitWidths[ele,:,:,:] = dfftfpconvfloat
+
+
+flagDfftPowMean = 1
+if flagDfftPowMean:
+    dfftSpecdB = 10*np.log10(np.mean(np.abs(dfft[targetRangeBins,:,:])**2,axis=(2)))
+
+    dfftfpconvfloatSpec = np.mean(np.abs(dfftfpconvfloatAllBitWidths[:,targetRangeBins,:,:])**2,axis=(3))
+    dfftfpconvfloatSpecdB = 10*np.log10(dfftfpconvfloatSpec)
+else:
+    dfftSpecdB = 10*np.log10(np.abs(dfft[targetRangeBins,:,0])**2)
+
+    dfftfpconvfloatSpecdB = 10*np.log10(np.abs(dfftfpconvfloatAllBitWidths)**2)
+    dfftfpconvfloatSpecdB = dfftfpconvfloatSpecdB[:,targetRangeBins,:,0]
+
+doppnoiseFloordB = noiseFloordB - 10*np.log10(numChirps)
+
 
 
 plt.figure(1,figsize=(20,10))
@@ -93,11 +122,32 @@ for ele in range(numTestCases):
     plt.subplot(2,3,ele+1)
     plt.title('{} bit RFFT'.format(numBitsRangeFFTOutput[ele]))
     plt.plot(rfftSpecdB,label='Without FFT quantization')
-    plt.plot(rfftfpconvfloatSpecdB[ele],lw=2,alpha=0.5,label='With {} bit FFT quantization'.format(numBitsRangeFFTOutput[ele]))
-    plt.axhline(theoretRangeFloorValQuant[ele],color='k',ls='dashed',label='Quant floor due to {} bit RFFT'.format(numBitsRangeFFTOutput[ele]))
+    plt.plot(rfftfpconvfloatSpecdB[ele,:],lw=2,alpha=0.5,label='With {} bit FFT quantization'.format(numBitsRangeFFTOutput[ele]))
     plt.axhline(noiseFloordB,color='k',ls='dotted',label='Programmed noise floor')
+    plt.axhline(theoretRangeFloorValQuant[ele],color='k',ls='dashed',label='Quant floor due to {} bit RFFT'.format(numBitsRangeFFTOutput[ele]))
     plt.xlabel('Range bins')
     plt.ylabel('dBm')
     plt.legend()
     plt.grid(True)
     plt.ylim(min(min(theoretRangeFloorValQuant),noiseFloordB)-10, 10)
+
+
+doppLegend = ['Target 1 without Dopp FFT quant', 'Target 2 without Dopp FFT quant',
+              'Target 1 with Dopp FFT quant', 'Target 2 with Dopp FFT quant', 'Expected Dopp noise floor']
+
+plt.figure(2,figsize=(20,10))
+plt.suptitle('Doppler spectrum')
+for ele in range(numTestCases):
+    plt.subplot(2,3,ele+1)
+    plt.title('{} bit DFFT'.format(numBitsRangeFFTOutput[ele]))
+    plt.plot(dfftSpecdB.T)
+    plt.plot(dfftfpconvfloatSpecdB[ele,:,:].T,lw=2,alpha=0.5)
+    plt.axhline(doppnoiseFloordB,color='k',ls='dotted')
+    plt.axhline(theoretRangeFloorValQuant[ele],color='k',ls='dashed')
+    plt.xlabel('Doppler bins')
+    plt.ylabel('dBm')
+    doppLegendFull = doppLegend + ['Quant floor due to {} bit DFFT'.format(numBitsRangeFFTOutput[ele])]
+    plt.legend(doppLegendFull)
+    plt.grid(True)
+    plt.ylim(min(min(theoretRangeFloorValQuant),doppnoiseFloordB)-10, 10)
+
