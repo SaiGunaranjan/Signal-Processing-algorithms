6d2de8db5e24f7035e55020dca35ec5427afc651
Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>
2024-04-14 20:45:14 +0530

    RFFT bitwidth vs quantization floor
    In this commit, I have created a script to analyze the impact of different bitwidths at the Range FFT output. For example how does the range FFT floor compare for a 8 bit vs 16 bit vs 24 bit RFFT output. We need the quantization floor to be well below the thermal floor so that the SNR is not limited, else the quantization floor due to the LSB will start limiting due to underflows. The smallest absolute value for RFFT output with B fractional bits is 2**-B (1 LSB value). Hence the power spectrum will have 2B fractional bits and hence the smallest power value that can be represented is 2**(2*-B). This sets the noise floor(in linear scale). On log scale it is 10*np.log10(2**(2*-B)). So, all power values less than 2**(-2B) will underflow to 0.
    
    I will be examining the impact of this qunatization and design a system with the optimal bitwidth to cater to range FFT specs and parameters such as system gain, detection theshold, etc.
    
    Signed-off-by: Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>

---
+++ fixed_point_coding/bitwidth_dynamicrange_analysis.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 12 23:06:16 2024
+
+@author: Sai Gunaranjan
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.close('all')
+
+numADCBits = 16
+numADCFracBits = numADCBits-1
+
+numBitsRangeFFTOutput = 12-1 # -1 for the integer/sign bit
+
+numADCSamples = 2048
+numRangeSamples = numADCSamples//2
+numChirps = 512
+numRxs = 4
+""" Noise parameters"""
+thermalFloor = -174 # dBm/Hz
+rxGain = 38 #dB
+NF = 9 # dB
+adcSamplRate = 56.25e6 # 56.25 MHz
+rbw = 10*np.log10(adcSamplRate/numADCSamples)
+# noiseFloordB = thermalFloor + rbw + rxGain + NF
+noiseFloordB = -90 # bin snr
+noise_power_db = noiseFloordB + 10*np.log10(numADCSamples)
+noise_variance = 10**(noise_power_db/10)
+noise_sigma = np.sqrt(noise_variance)
+
+object_snr = np.array([60,10]) #np.array([60,-10])
+weights = np.sqrt(10**((noiseFloordB + object_snr)/10))
+
+targetRangeBins = np.array([20,800])
+targetDopplerBins = np.array([0,200])
+targetAngleBins = np.array([0,2])
+
+rangeSignal = np.exp(1j*2*np.pi*targetRangeBins[:,None]*np.arange(numADCSamples)[None,:]/numADCSamples)
+dopplerSignal = np.exp(1j*2*np.pi*targetDopplerBins[:,None]*np.arange(numChirps)[None,:]/numChirps)
+angleSignal = np.exp(1j*2*np.pi*targetAngleBins[:,None]*np.arange(numRxs)[None,:]/numRxs)
+
+signalComponents = weights[:,None,None,None] * rangeSignal[:,:,None,None] * dopplerSignal[:,None,:,None] * angleSignal[:,None,None,:]
+cleanSignal = np.sum(signalComponents,axis=0)
+
+noise = (noise_sigma/np.sqrt(2))*np.random.randn(numADCSamples * numChirps * numRxs) + \
+    1j*(noise_sigma/np.sqrt(2))*np.random.randn(numADCSamples * numChirps * numRxs)
+
+noise = noise.reshape(numADCSamples,numChirps,numRxs)
+
+radarSignal = cleanSignal + noise
+
+adcRadarSignal = np.floor(radarSignal.real * 2**numADCFracBits + 0.5) + \
+    1j*np.floor(radarSignal.imag * 2**numADCFracBits + 0.5)
+
+rfft = np.fft.fft(adcRadarSignal,axis=0)/(numADCSamples* 2**numADCFracBits)
+rfft = rfft[0:numRangeSamples,:,:]
+
+
+
+rfftfp = np.floor(rfft.real * 2**numBitsRangeFFTOutput + 0*0.5) + \
+    1j*np.floor(rfft.imag * 2**numBitsRangeFFTOutput + 0*0.5)
+
+rfftfpconvfloat = rfftfp/(2**numBitsRangeFFTOutput)
+rfftfpconvfloatSpec = np.mean(np.abs(rfftfpconvfloat)**2,axis=(1,2))
+
+flagRfftPowMean = 1
+
+if flagRfftPowMean:
+    rfftSpecdB = 10*np.log10(np.mean(np.abs(rfft)**2,axis=(1,2)))
+
+    rfftfpconvfloatSpecdB = 10*np.log10(rfftfpconvfloatSpec)
+else:
+    rfftSpecdB = 10*np.log10(np.abs(rfft[:,0,0])**2)
+
+    rfftfpconvfloatSpecdB = 10*np.log10(np.abs(rfftfpconvfloat)**2)
+    rfftfpconvfloatSpecdB = rfftfpconvfloatSpecdB[:,0,0]
+
+
+
+
+
+
+theoretRangeFloorValQuant = 10*np.log10(2**(2*-numBitsRangeFFTOutput))
+
+plt.figure(1,figsize=(20,10),dpi=200)
+plt.title('Range spectrum')
+plt.plot(rfftSpecdB,label='Without FFT quantization')
+plt.plot(rfftfpconvfloatSpecdB,lw=2,alpha=0.5,label='With {} bit FFT output'.format(numBitsRangeFFTOutput+1))
+plt.axhline(theoretRangeFloorValQuant,color='k',ls='dashed',label='Quant floor due to {} bit RFFT quantization'.format(numBitsRangeFFTOutput+1))
+plt.axhline(noiseFloordB,color='k',ls='dotted',label='Programmed noise floor')
+plt.xlabel('Range bins')
+plt.ylabel('dBm')
+plt.legend()
+plt.grid(True)
+plt.ylim(min(theoretRangeFloorValQuant,noiseFloordB)-10, 10)
